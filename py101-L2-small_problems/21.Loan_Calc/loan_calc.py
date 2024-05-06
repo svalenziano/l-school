162d9063565c9a732ab91b16fed17d65a86c64ba
SValenziano <senorvalenz@gmail.com>
2024-05-06 15:52:53 -0400

    loan_calc.py Working

--- py101-L2-small_problems/21.Loan_Calc/loan_calc.py
+++ py101-L2-small_problems/21.Loan_Calc/loan_calc.py
@@ -3,29 +3,67 @@ m = p * (j / (1 - (1 + j) ^^ (-n)))
 
 
 '''
-loan_amt = None
-monthly_interest_rate = None
-loan_duration_months = None
+# MESSAGES
+invalid_no = "Invalid number, please try again."
 
-monthly_payment = loan_amt * (monthly_interest_rate / (1 - 
-                  (1 + monthly_interest_rate) ** (-1 * loan_duration_months)))
+def prompt(message):
+    x = input("==> " + message + '\n')
+    return x
+
+def valid_number(number):
+    try:
+        float(number)
+    except ValueError:
+        return False
+    
+    return True
+
+def format_number(num_string):
+    for i in [',' , '_', '$', " ",'%']:
+        num_string = num_string.replace(i,"")
+        return num_string
+
+while True:
+
+    # Get loan amt from user
+
+    loan_amt = prompt("How much $ would you like to borrow?") or "$20,000"
+    loan_amt = format_number(loan_amt)
+
+    # validate: Positive $ amount
+    while not (valid_number(loan_amt) and (float(loan_amt) > 0)):
+        loan_amt = prompt("Invalid number, please try again. "
+                          "An example of a valid number is $20,000")
+
+    # print(loan_amt)
+
+    # Get APR from user as %
+    apr = prompt("What is your APR? Example: 5.5%. "
+                                   "(not to be confused with APY)") or "5.5%"
+    apr = format_number(apr)
+
+    while not (valid_number(apr) and float(apr) < 0):
+        apr = prompt('''Invalid APR, please try again.  A couple of examples:
+                     5%
+                     6.3''')
+    
+    monthly_interest_rate = apr/12
+
+    breakpoint()
+    #monthly_interest_rate = APR/12
 
 
-# Get loan amt from user
-# validate: Positive $ amount
-
-# Get APR from user as %
-#
-# validate: 
-    # positive number under 50
-    # 
-
-monthly_interest_rate = APR/12
-# Get loan duration from user
-# validate
-# print in days/years
+    # Get loan duration from user
+    # validate
+    # print in days/years
 
 
-# calculate
+    # calculate
 
-# Output
+    # Output
+
+    '''
+    monthly_payment = loan_amt * (monthly_interest_rate / (1 - 
+                    (1 + monthly_interest_rate) ** (-1 * loan_duration_months)))
+    '''
+    print ("loop finished, starting over \n" , '*'*20)
