74b1d702047581e45d9012e100915d902d8b4d8c
SValenziano <senorvalenz@gmail.com>
2024-05-06 11:27:26 -0400

    Working, prior to exercise 21

---
+++ py101-L2-small_problems/21.Loan_Calc/loan_calc.py
@@ -0,0 +1,26 @@
+'''
+m = p * (j / (1 - (1 + j) ^^ (-n)))
+
+
+'''
+loan_amt = None
+monthly_interest_rate = None
+loan_duration_months = None
+
+monthly_payment = loan_amt * (monthly_interest_rate / (1 - 
+                  (1 + monthly_interest_rate) ** (-1 * loan_duration_months)))
+
+
+# Get loan amt from user
+# validate
+
+# Get APR from user
+# validate
+
+# Get loan duration from user
+# validate
+
+
+# calculate
+
+# Output
