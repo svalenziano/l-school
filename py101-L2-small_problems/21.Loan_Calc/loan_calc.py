324b6e97ec4ec00c2f41d3f97ca3162792ccca5a
SValenziano <senorvalenz@gmail.com>
2024-05-07 16:30:24 -0400

    Working loan_calc.py, prior to checking Solution

--- py101-L2-small_problems/21.Loan_Calc/loan_calc.py
+++ py101-L2-small_problems/21.Loan_Calc/loan_calc.py
@@ -18,52 +18,87 @@ def valid_number(number):
     
     return True
 
-def format_number(num_string):
-    for i in [',' , '_', '$', " ",'%']:
-        num_string = num_string.replace(i,"")
-        return num_string
+def clean_number(num_string):
+    naughty = [',' , '_', '$', " ",'%', '+']
+    return ''.join(char for char in num_string if (char not in naughty))
+
+print("*** WELCOME TO LOAN-CALC 3000! ***")
 
 while True:
 
     # Get loan amt from user
 
     loan_amt = prompt("How much $ would you like to borrow?") or "$20,000"
-    loan_amt = format_number(loan_amt)
+    loan_amt = clean_number(loan_amt)
 
     # validate: Positive $ amount
     while not (valid_number(loan_amt) and (float(loan_amt) > 0)):
         loan_amt = prompt("Invalid number, please try again. "
                           "An example of a valid number is $20,000")
-
-    # print(loan_amt)
+        loan_amt = clean_number(loan_amt)
+    loan_amt = float(loan_amt)
 
     # Get APR from user as %
     apr = prompt("What is your APR? Example: 5.5%. "
-                                   "(not to be confused with APY)") or "5.5%"
-    apr = format_number(apr)
+                "(not to be confused with APY)") or "5.5%"
+    apr = clean_number(apr)
 
-    while not (valid_number(apr) and float(apr) < 0):
+    # Validate
+    while not (valid_number(apr) and (float(apr) >= 0)):
         apr = prompt('''Invalid APR, please try again.  A couple of examples:
                      5%
                      6.3''')
+        apr = clean_number(apr)
     
-    monthly_interest_rate = apr/12
+    apr = float(apr) * 0.01   # Convert to percentage (5.5% ==> 0.055)
 
-    breakpoint()
-    #monthly_interest_rate = APR/12
+    # Convert APR to monthly interest rate
+    monthly_interest_rate = float(apr)/12
 
-
-    # Get loan duration from user
-    # validate
-    # print in days/years
+    # Get loan duration from user, in months
+    months = prompt("What's the duration of the loan in months?") or '24'
+    months = clean_number(months)
+    
+    while not (valid_number(months) and 
+               (float(months) % 1 == 0) and 
+               (int(months) > 0)):
+        months = prompt("Invalid number, please try again."
+                        " A couple of examples: 4 or 24 mo."
+                        " Whole numbers only, please."
+                        )
+        months = clean_number(months)
+    
+    months = int(months)
 
 
     # calculate
+    if apr > 0:
+        monthly_payment = (
+                loan_amt * (monthly_interest_rate / 
+                (1 - (1 + monthly_interest_rate) ** (-1 * months)))
+                )
+    else:
+        monthly_payment = loan_amt / months
+    
+    rounded_monthly_payment = round(monthly_payment, 2)
+    
+    total_payment = monthly_payment*months
+
+    # Notes on the following print statment:
+    # apr*100 since APR's are usually listed as, for example, 5.5%, not 0.055
+    print(
+f'''
+INPUTS:
+Borrowing: ${loan_amt:,.2f}
+APR: {apr*100}% 
+Loan Duration: {months} months
+
+RESULTS:
+Monthly Payment: ${rounded_monthly_payment:,.2f}
+Total Payments: ${total_payment:,.2f}
+''' )
 
     # Output
 
-    '''
-    monthly_payment = loan_amt * (monthly_interest_rate / (1 - 
-                    (1 + monthly_interest_rate) ** (-1 * loan_duration_months)))
-    '''
-    print ("loop finished, starting over \n" , '*'*20)
+    # print ("loop finished, starting over \n" , '*'*20)
+    break
