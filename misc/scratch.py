1c0f2fdb079a3ada867cbdc2c76446790f194580
SValenziano <senorvalenz@gmail.com>
2024-06-11 15:45:43 -0400

    WIP

--- misc/scratch.py
+++
@@ -1,9 +0,0 @@
-def longest_word(sentence):
-    words = sentence.split()
-    saved_word = words.pop(0)
-
-    for word in words:
-        if len(saved_word) < len(word):
-            saved_word = word
-
-    return saved_word
