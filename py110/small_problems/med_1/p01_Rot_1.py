67f6471f4562df98077945867c23dd98054e8d27
SValenziano <senorvalenz@gmail.com>
2024-07-23 15:44:23 -0400

    Try 2, easy

--- py110/small_problems/med_1/p01_Rot_1.py
+++ py110/small_problems/med_1/p01_Rot_1.py
@@ -1,34 +1,10 @@
-'''
-P
-    INPUT = list
-    Return = MOVE FIRST ELEMENT TO THE END OF THE LIST.  Return new list.
-        Do not mutate.
-    Reqs
-        Ex
-            If input = empty, return empty list
-            If input is not a list, return None
-E
-D
-A
-    - If input isn't list
-        - Return None
-    - Deal with empty list?
-    - INIT list as a deepcopy of the orig list
-    - pop the first element and append it to the end of the first list
-    - return the new list
-C
-'''
-from copy import deepcopy
-
-def rotate_list(orig_lst):
-    if not isinstance(orig_lst, list):
+def rotate_list(lst):
+    if not isinstance(lst,list):
         return None
-    if not orig_lst:
-        return []
-    new_lst = deepcopy(orig_lst)
-    last = new_lst.pop(0)
-    new_lst.append(last)
-    return new_lst
+    if lst:
+        lst = lst[1:] + [lst[0]]
+    return lst
+
 
 # All of these examples should print True
 
