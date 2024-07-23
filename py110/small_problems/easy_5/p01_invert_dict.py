c14685720cf86ea61e9102a6bfae0d989385caa3
SValenziano <senorvalenz@gmail.com>
2024-07-23 13:57:57 -0400

    Try 2, using comprehension

--- py110/small_problems/easy_5/p01_invert_dict.py
+++ py110/small_problems/easy_5/p01_invert_dict.py
@@ -1,10 +1,17 @@
-def invert_dict_v01(my_dict):
-    return dict(zip(my_dict.values(), my_dict.keys()))
+'''
+INPUT = dict
+OUTPUT = inverted dict; that is, swap keys and values.
+REQS
+    ex
+        All values are unique
+'''
+
 
 def invert_dict(my_dict):
-    return {value:key for key, value in my_dict.items()}
+    return {value: key for key, value in my_dict.items()}
 
 
+# LS TEST:
 print(invert_dict({
           'apple': 'fruit',
           'broccoli': 'vegetable',
