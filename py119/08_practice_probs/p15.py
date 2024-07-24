0edf3e4eecc7988e6fac3100ada8be403a9c2c8c
SValenziano <senorvalenz@gmail.com>
2024-07-24 15:03:14 -0400

    Try 2, easy!

--- py119/08_practice_probs/p15.py
+++ py119/08_practice_probs/p15.py
@@ -1,43 +1,51 @@
 '''
-RESULT: 🔴 I thought it would be easy, but it turned into a train wreck. 
-            Aborted at 24 mins.
+RESULT = 🟢 14.5 mins, pretty easy :)
 
 
 P
-    INPUT = string, numeric digits only, minimum length = 5
-    OUTPUT = integer, greatest product of four consecutive digits in the string
-    REQ
+    input = string
+    output = greatest product of four consecutive digits
+    reqs
+        ex
+            arg will always have more than 4 digits
+        imp
 E
-    '23456', len = 5
-    ['2345' , '3456']
-    start = 0, 1
-    end = 3, 4
-    start of 1 --> end of 5
+
 D
+    list of products
 A
-    Get all possible strings (use comprehension)
-    Create list of sums of all those strings (comprehension)
-    Return the max sum    
+    Create list of all possible substrings of length 4
+    Create list of products
+    Return max product
+
+    [HELPER] products
+    INPUT = STRING
+    OUTPUT = product
+    Alg:
+        - split string into list of ints (comprehension)
+        - calc product using for loop
+        - return product
 '''
 
+def return_string_product(string):
+    nums = [int(char) for char in string]
+    product = 1
+    for num in nums:
+        product *= num
+    return product
+
 def greatest_product(string):
-    LENGTH = 4
-    substrings = [list(string[start_idx: end_idx])
-                  for start_idx in range(len(string) - LENGTH + 1)
-                  for end_idx in range(start_idx + LENGTH, len(string) + 1)
-                  if end_idx - start_idx == 4]
-    integers = [int(digit)
-                for sublist in substrings
-                for digit in sublist]
-    print(integers)
+    substrings = [string[start_idx: start_idx + 4]
+                  for start_idx in range(len(string) - 3)]
+    products = [return_string_product(substring)
+                for substring in substrings]
+    return max(products)
 
 
+def ls():
+    print(greatest_product('23456') == 360)      # 3 * 4 * 5 * 6
+    print(greatest_product('3145926') == 540)    # 5 * 9 * 2 * 6
+    print(greatest_product('1828172') == 128)    # 1 * 8 * 2 * 8
+    print(greatest_product('123987654') == 3024) # 9 * 8 * 7 * 6
 
-
-print(greatest_product('23456') == 360)      # 3 * 4 * 5 * 6
-print(greatest_product('3145926') == 540)    # 5 * 9 * 2 * 6
-print(greatest_product('1828172') == 128)    # 1 * 8 * 2 * 8
-print(greatest_product('123987654') == 3024) # 9 * 8 * 7 * 6
-
-
-
+ls()
