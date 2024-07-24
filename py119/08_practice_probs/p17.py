8c2f8973c0226640764a2aa0260a42c6e66ff891
SValenziano <senorvalenz@gmail.com>
2024-07-24 15:39:23 -0400

    Try 2, easy!

--- py119/08_practice_probs/p17.py
+++ py119/08_practice_probs/p17.py
@@ -1,65 +1,58 @@
 '''
-RESULT = 🟢 pretty easy although I didn't time myself :*(
+RESULT =  🟢 16 mins, pretty easy!
+            Worked pretty flawlessly.  Tested as I went!
+
 
 
 P
-    IN = list of ints
-    OUT = min int that can be appended to the list, sum lst must equal the closest
-        prime # that's greater than the sum of the ints
+    Input = list of ints
+    Output = integer, diff between the current sum of the numbers, and the nearest prime number greater
+                than the current sum.
 E
 D
+    ?
 A
-    [IS PRIME]
-        input = integer
-        output = bool, Is the number prime?
-        Reqs
-            Prime # = number with whose only divisors are one and itself
-        Alg
-            Set comprehension to determine multiples other than one and num:
-                divisor
-                for every divisor btw 2 and num - 1 (skip 1 and num)
-                if modulo num % divisor == 0 (divides evenly)
-            return True if the set is empty
-            otherwise, return False
-
-    [NEAREST PRIME]
-        INPUT = num
-        output = nearest prime # (greater than num)
-        Alg
-            while number isn't prime (see helper above)
-            increment num
-        
-
-    [MAIN FUNC]
-        Get sum of ints
-        Find nearest prime # (greater than sum)
-        Return diff
+    [MAIN]
+        Get sum of current list
+        Find next largest prime [HELPER]
+        Return diff btw prime and current sum
+    next_largest_prime [HELPER]
+        - add one to number
+        - while number is not prime
+            - add one to the number
+        - return the number
+    is_prime(num) [HELPER]
+        - PRIME = num that can only be divided by itself and one
+        - create list of nums with comprehension
+            - for factors between 1 and num (non-inclusive)
+            - if num % factor == 0
+        - return len(list) > 0
 '''
 
-def is_prime(integer):
-    factors = {divisor
-               for divisor in range(2, integer)
-               if integer % divisor == 0}
-    #print(integer, ' ==> ',factors)
-    return not bool(factors)
+def is_prime(num):
+    factors = [factor
+               for factor in range(2, num)
+               if num % factor == 0]
+    return len(factors) == 0
 
+def next_largest_prime(num):
+    num += 1
+    while not is_prime(num):
+        num += 1
+    return num
 
-def nearest_prime(integer):
-    integer += 1
-    while True:
-        if is_prime(integer):
-            return integer
-        integer += 1
+def nearest_prime_sum(lst):
+    return next_largest_prime(sum(lst)) - sum(lst)
 
-def nearest_prime_sum(lst_of_ints):
-    sum_ = sum(lst_of_ints)
-    return nearest_prime(sum_) - sum_
+#for num in [6, 7, 3, 32,]:
+#    print(num, next_largest_prime(num))
 
+def ls():
+    print(nearest_prime_sum([1, 2, 3]) == 1)        # Nearest prime to 6 is 7
+    print(nearest_prime_sum([5, 2]) == 4)           # Nearest prime to 7 is 11
+    print(nearest_prime_sum([1, 1, 1]) == 2)        # Nearest prime to 3 is 5
+    print(nearest_prime_sum([2, 12, 8, 4, 6]) == 5) # Nearest prime to 32 is 37
 
-print(nearest_prime_sum([1, 2, 3]) == 1)        # Nearest prime to 6 is 7
-print(nearest_prime_sum([5, 2]) == 4)           # Nearest prime to 7 is 11
-print(nearest_prime_sum([1, 1, 1]) == 2)        # Nearest prime to 3 is 5
-print(nearest_prime_sum([2, 12, 8, 4, 6]) == 5) # Nearest prime to 32 is 37
-
-# Nearest prime to 163 is 167
-print(nearest_prime_sum([50, 39, 49, 6, 17, 2]) == 4)
+    # Nearest prime to 163 is 167
+    print(nearest_prime_sum([50, 39, 49, 6, 17, 2]) == 4)
+ls()
