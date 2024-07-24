22f0bdcf0a6c7f8b04aab05d2b15862aacfcd57a
SValenziano <senorvalenz@gmail.com>
2024-07-24 10:25:38 -0400

    Try 2, easy!

--- py110/small_problems/adv/p01_transpose_3x3.py
+++ py110/small_problems/adv/p01_transpose_3x3.py
@@ -1,47 +1,43 @@
 '''
+RESULT 🟢 not bad!  11.5 mins.  Had to fix my initial error
+
 P
-    INPUT = matrix (nested list)
-    OUTPUT = transposed matrix
-    REQ
-        EX
-            - Create a new object (don't mutate the original)
-        IMPL
-            - All nested lists are of equal length
+    INPUT = nxn matrix
+    OUTPUT = TRANSPOSED MATRIX (column becomes row)
 E
 D
-    ?
+    new nested list(s)
 A
-    V1, high level
-        - For each column (a single index for each row)
-            - Create list of 'column' elements (now it's a row)
-            - Append the row (formerly a column) onto the list
-        - Return the matrix
-        - CONCLUSION: I started coding and got stuck.  Back to the drawing board.
+    v1 high level
+        - for each 'column', generate a row (list)
+    
+    v1 low level
+        - comprehension
+            - sublist item at index
+            - for each index btw 0 and length of first sublist
 
-    V2, see sketch
-    
-    
-    SIMPLIFY?
-C
 '''
+def transpose(matrix):
+    width = len(matrix[0])
+    height = len(matrix)
+    result = [[matrix[row_idx][col_idx] 
+               for row_idx in range(height)]
+               for col_idx in range(width)]
+
+    print(result)
+    return result
 
 
-def transpose(orig_matrix):
-    transposed = []
-    number_of_columns = range(len(orig_matrix[0]))
-    for column_index in number_of_columns:
-        orig_column = [row[column_index] for row in orig_matrix]
-        transposed.append(orig_column)
-    return transposed
+def ls():
+    matrix = [
+        [1, 5, 8],
+        [4, 7, 2],
+        [3, 9, 6],
+    ]
 
+    new_matrix = transpose(matrix)
 
-matrix = [
-    [1, 5, 8],
-    [4, 7, 2],
-    [3, 9, 6],
-]
+    print(new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]) # True
+    print(matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]])     # True
 
-new_matrix = transpose(matrix)
-
-print(new_matrix == [[1, 4, 3], [5, 7, 9], [8, 2, 6]]) # True
-print(matrix == [[1, 5, 8], [4, 7, 2], [3, 9, 6]])     # True
+ls()
