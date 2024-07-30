fa1406cfb4e269f0b9c4fb8b14510bf7ee3ccc44
SValenziano <senorvalenz@gmail.com>
2024-07-30 15:34:28 -0400

    After move

--- py120/02_oo_programming/05_inheritance_small_probs/01.py
+++ py120/02_oo_programming/05_inheritance_small_probs/01.py
@@ -12,6 +12,14 @@ class Vehicle:
     def year(self):
         return self._year
 
+class Car(Vehicle):
+    def __init__(self, year):
+        super().__init__(year)
+
+class Truck(Vehicle):
+    def __init__(self, year):
+        super().__init__(year)
+
 # Comments show expected output
 truck1 = Truck(1994)
 print(truck1.year)            # 1994
