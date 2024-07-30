e7b4a84f7ea3ffea07dc8aeddeb0cb0682f2692a
SValenziano <senorvalenz@gmail.com>
2024-07-30 15:38:25 -0400

    WIP

---
+++ py120/02_oo_programming/05_inheritance_small_probs/02.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    def __init__(self, year):
+        self._year = year
+
+    @property
+    def year(self):
+        return self._year
+
+class Truck(Vehicle):
+    
+    def __init__(self, year):
+        super().__init__(year)
+        self.start_engine()
+    
+    def start_engine(self):
+        print('Ready to go!')
+
+# Comments show expected output
+truck1 = Truck(1994)          # Ready to go!
+print(truck1.year)            # 1994
