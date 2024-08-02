28d306aa300a23cd6502977edd7a9fd425a19d8b
SValenziano <senorvalenz@gmail.com>
2024-08-02 13:54:38 -0400

    Working!

--- py120/small_probs/medium/p01_circular_buffer.py
+++ py120/small_probs/medium/p01_circular_buffer.py
@@ -71,11 +71,12 @@ A
 '''
 
 # SV Code
+# Obj is short for 'Object', as in, a Python Object
 class Obj:
-    def __init__(self, idx):
+    def __init__(self, idx, value=None):
         self._idx = idx
-        self.value = None
-        self.age = None
+        self.value = value
+        self._age = None
     
     def __repr__(self):
         return str(self.value)
@@ -96,17 +97,13 @@ class Obj:
     def age(self):
         return self._age
 
-    @age.setter
-    def age(self, age):
-        self._age = age
-
     def increment_age(self):
         # only increment if there's a value
         if self.value != None:
             if self.age == None:
-                self.age = 1
+                self._age = 1
             else:
-                self.age += 1
+                self._age += 1
 
     
     def __lt__(self, other):
@@ -148,49 +145,79 @@ class CircularBuffer:
         return True                
 
     @property
-    def newest_index(self):
+    def idx_after_newest_idx(self):
         sorted_buffer = self.sorted_by_age
         if sorted_buffer:
-            return sorted_buffer[0].idx
+            newest_idx = sorted_buffer[0].idx
+            if newest_idx + 1 >= len(self.buffer):
+                return 0
+            return newest_idx + 1
         return 0
     
     @property
-    def oldest_index(self):
+    def oldest_idx(self):
         sorted_buffer = self.sorted_by_age
         if sorted_buffer:
             return sorted_buffer[-1].idx
         return 0
+    
+    def update_value(self, idx, value):
+        self._buffer[idx].value = value
+    
+    def replace_object(self, idx, object):
+        self._buffer[idx] = object
+
+    def remove_object(self, idx):
+        '''
+        Replaces the object with an 'empty' Obj
+        (Doesn't actually remove the object)
+        '''
+        self._buffer[idx] = Obj(idx)
         
-    def put(self, value):
+    def put(self, new_value):
         if self.is_empty:
-            self._buffer[0].value = value
+            self.update_value(idx = 0, 
+                              value = new_value)
         elif self.is_full:
-            self._buffer[self.oldest_index].value = value
+            oldest_idx = self.oldest_idx
+            self.replace_object(idx=oldest_idx, 
+                                object=Obj(idx=oldest_idx, value=new_value))
         else:
-            idx_to_modify = self.newest_index + 1
-            self._buffer[idx_to_modify].value = value
-        for cell in self.buffer:
-                cell.increment_age()
+            self.update_value(idx = self.idx_after_newest_idx,
+                              value = new_value)
+        for obj in self.buffer:
+                obj.increment_age()
+    
+    def get(self):
+        old_idx = self.oldest_idx
+        oldest_value = self.buffer[old_idx].value
+        self.remove_object(old_idx)
+        return oldest_value
+
     
 
         
 
 # SV Tests
-buffer = CircularBuffer(3)
-# print(f"Buffer is empty? {buffer.all_cells_empty}")
-# print(b.buffer)
-# print(b.sorted_by_age)
-# print(b.is_full)
-# print(b.newest_index)
-buffer.put(1)   # 1 _ _
-# print(f"Buffer is empty? {buffer.all_cells_empty}")
-print(buffer)
+# buffer = CircularBuffer(3)
+# # print(f"Buffer is empty? {buffer.all_cells_empty}")
+# # print(b.buffer)
+# # print(b.sorted_by_age)
+# # print(b.is_full)
+# # print(b.newest_index)
+# buffer.put(1)   # 1 _ _
+# # print(f"Buffer is empty? {buffer.all_cells_empty}")
+# print(buffer)
 
-buffer.put(2)   # 1 2 _
-# print(f"Buffer is empty? {buffer.all_cells_empty}")
-buffer.put(3)
-buffer.put(4)
-print(buffer)
+# buffer.put(2)   # 1 2 _
+# print(buffer)
+
+# # print(f"Buffer is empty? {buffer.all_cells_empty}")
+# buffer.put(3)
+# print(buffer)
+
+# buffer.put(4)
+# print(buffer)
 # print(buffer.get() == 1)   # _ 2 _            # True
 
 # buffer.put(3)  # _ 2 3
@@ -209,43 +236,43 @@ print(buffer)
 
 
 # LS Tests
-# buffer = CircularBuffer(3)
+buffer = CircularBuffer(3)
 
-# # print(buffer.get() is None)          # True
-
-# buffer.put(1)   # 1 _ _
-# buffer.put(2)   # 1 2 _
-# print(buffer.get() == 1)   # _ 2 _            # True
-
-# buffer.put(3)  # _ 2 3
-# buffer.put(4)  # 4 2 3
-# print(buffer.get() == 2)  # 4 3            # True
-
-# buffer.put(5)
-# buffer.put(6)
-# buffer.put(7)
-# print(buffer.get() == 5)             # True
-# print(buffer.get() == 6)             # True
-# print(buffer.get() == 7)             # True
 # print(buffer.get() is None)          # True
 
-# buffer2 = CircularBuffer(4)
+buffer.put(1)   # 1 _ _
+buffer.put(2)   # 1 2 _
+print(buffer.get() == 1)   # _ 2 _            # True
 
-# print(buffer2.get() is None)         # True
+buffer.put(3)  # _ 2 3
+buffer.put(4)  # 4 2 3
+print(buffer.get() == 2)  # 4 3            # True
 
-# buffer2.put(1)
-# buffer2.put(2)
-# print(buffer2.get() == 1)            # True
+buffer.put(5)
+buffer.put(6)
+buffer.put(7)
+print(buffer.get() == 5)             # True
+print(buffer.get() == 6)             # True
+print(buffer.get() == 7)             # True
+print(buffer.get() is None)          # True
 
-# buffer2.put(3)
-# buffer2.put(4)
-# print(buffer2.get() == 2)            # True
+buffer2 = CircularBuffer(4)
 
-# buffer2.put(5)
-# buffer2.put(6)
-# buffer2.put(7)
-# print(buffer2.get() == 4)            # True
-# print(buffer2.get() == 5)            # True
-# print(buffer2.get() == 6)            # True
-# print(buffer2.get() == 7)            # True
-# print(buffer2.get() is None)         # True
+print(buffer2.get() is None)         # True
+
+buffer2.put(1)
+buffer2.put(2)
+print(buffer2.get() == 1)            # True
+
+buffer2.put(3)
+buffer2.put(4)
+print(buffer2.get() == 2)            # True
+
+buffer2.put(5)
+buffer2.put(6)
+buffer2.put(7)
+print(buffer2.get() == 4)            # True
+print(buffer2.get() == 5)            # True
+print(buffer2.get() == 6)            # True
+print(buffer2.get() == 7)            # True
+print(buffer2.get() is None)         # True
