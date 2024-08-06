63cfc0cf04d4875791421a4f61f52ba53dd02859
SValenziano <senorvalenz@gmail.com>
2024-08-06 10:53:53 -0400

    sv solution

--- py120/small_probs/medium/p05_card_deck.py
+++ py120/small_probs/medium/p05_card_deck.py
@@ -1,3 +1,5 @@
+from random import shuffle
+
 '''
 P
     - Create a deck with the 'Cards' from the previous problem
@@ -7,14 +9,59 @@ P
             from new cards
 E
 D
+    - Deck
+        STATE:
+            - List of cards (shuffled)
+            - Discard pile
+        BEHAVIORS
+            - __init__
+            - `draw`
+            - `start_over`
 A
 '''
-# start here
+# MY CODE
 class Deck:
     RANKS = list(range(2, 11)) + ['Jack', 'Queen', 'King', 'Ace']
     SUITS = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 
+    def __init__(self):
+        self.cards = self.shuffled_deck()
+    
+    def shuffled_deck(self):
+        cards = [Card(rank, suit)
+                      for suit in Deck.SUITS
+                      for rank in Deck.RANKS]
+        shuffle(cards)
+        return cards
+    
+    def draw(self):
+        if len(self.cards) < 1:
+            self.cards = self.shuffled_deck()
+        return self.cards.pop()
 
+
+class Card:
+    
+    VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
+    
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self._value = self.rank_to_value(rank)
+
+    def rank_to_value(self, rank):
+        for idx, value in enumerate(Card.VALUES, start=2):
+            if rank == value:
+                return idx
+    
+    def __lt__(self, other):
+        return self._value < other._value
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+    
+    def __eq__(self, other):
+        return (self.rank == other.rank) and (self.suit == other.suit)
 
 # LS TESTS
 deck = Deck()
