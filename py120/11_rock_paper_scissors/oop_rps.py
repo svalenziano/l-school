378c2ebf8b22996028c14a4c4064af81a87312b1
SValenziano <senorvalenz@gmail.com>
2024-08-06 16:09:04 -0400

    Ran into dead end with `display_winner`

--- py120/11_rock_paper_scissors/oop_rps.py
+++ py120/11_rock_paper_scissors/oop_rps.py
@@ -1,10 +1,27 @@
+import random
+from valid_input import Input
+
+
 class Player:
-    def __init__(self):
+    CHOICES = ('rock', 'paper', 'scissors')
+    
+    def __init__(self, is_human):
         # maybe a "name"? what about a "move"?
-        pass
+        self._is_human = is_human
+        self.move = None
 
     def choose(self):
-        pass
+        help_txt = f'You need help.  The valid choices are: R, P, or S'
+        if self._is_human:
+            get_move = Input(valid_choices = ['rock', 'paper', 'scissors'], 
+                      msg_txt = 'Please choose: rock, paper, or scissors? ', 
+                      invalid_txt = 'Not valid, sorry!', 
+                      help_txt = help_txt, 
+                      delay = 0
+                      )
+            self.move = get_move.get()
+        else:
+            self.move = random.choice(Player.CHOICES)
 
 class Move:
     def __init__(self):
@@ -22,22 +39,59 @@ class Rule:
         pass
 
 class RPSGame:
+
+    WINNING_HANDS = {
+        # keys = winners
+        'rock': {'scissors': 'Rock smashes scissors!'},
+        'paper': {'rock': 'Paper smothers rock!'},
+        'scissors': {'paper': 'Scissors cuts paper!'},
+    }
+
     def __init__(self):
-        self._human = Player()
-        self._computer = Player()
+        self._human = Player(is_human=True)
+        self._computer = Player(is_human=False)
+        print('init!')
 
     def display_welcome_message(self):
         print('Welcome to rock, paper scissors!')
 
     def display_goodbye_message(self):
-        print("Goodbye!")
+        print("Goodbye! 👋")
 
-    def display_winner(sef):
-        pass    
+    def display_winner(self):
+        '''
+        DS:
+            scissors : 
+                paper: 'scissors cuts paper'
+            rock :
+                scissors: 'rock crushes scissors'
+            paper :
+                rock: 'paper smothers rock'
+        Alg:
+            - If they're equal, it's a tie
+            - If the opposing move is in the value
+                - player wins
+            - Otherwise
+                - player loses
+        '''
+        print(f"Human chose {self._human.move}, Computer chose {self._computer.move}")
+        if self._human.move == self._computer.move:
+            print(f"It's a tie!")
+        else:
+            outcome = self.WINNING_HANDS[self._human.move][self._computer.move]
+            if self._computer.move in self.WINNING_HANDS[self._human.move]:
+                print(f'{outcome} Human wins!')
+            else:
+                print(f'{outcome} Computer wins')
+        
 
     def play(self):
-        self.display_welcome_message()
-        self._human.choose()
-        self._computer.choose()
-        self.display_winner()
-        self.display_goodbye_message()
+        print(self.WINNING_HANDS['rock']['scissors'])
+        # self.display_welcome_message()
+        # self._human.choose()
+        # self._computer.choose()
+        # self.display_winner()
+        # self.display_goodbye_message()
+
+game = RPSGame()
+game.play()
---
+++ py120/11_rock_paper_scissors/valid_input.py
@@ -0,0 +1,76 @@
+
+class Input:
+
+    HELP_OPTIONS = ['h', 'help', '?,']
+    QUIT_OPTIONS = ['q', 'quit']
+    PROMPT = '>>>'
+
+    '''
+    init:
+        Valid Choices
+        Msg Text
+        Invalid input text
+        Help text
+        Delay
+    Behaviors
+        msg to validate
+    Loop:
+        - while True
+            - display messages
+            - return valid choice
+    '''
+    def __init__(self, 
+                 valid_choices=[], 
+                 msg_txt='', 
+                 invalid_txt='', 
+                 help_txt='', 
+                 delay=0):
+        self.valid_choices = valid_choices
+        self.msg_txt = msg_txt
+        self.invalid_txt = invalid_txt
+        self.help_txt = help_txt
+        self.delay = delay
+        self.goodbye = 'Goodbye!'
+    
+    def unalias_choice(self, valid_choices, choice):
+        '''
+        For each alias,
+            Create a list of all valid_choices that start with that string
+            If the length is 1
+                Return the  valid_choice
+            Otherwise
+                Return invalid
+        '''
+        unaliased = [valid_choice for valid_choice in valid_choices
+                     if valid_choice.startswith(choice)]
+        if len(unaliased) == 1:
+            return unaliased[0]
+        if len(unaliased > 1):
+            print(f"There's more than one choice that begins with {choice}, " +
+                  "can you be more specific?")
+
+    def get(self):
+        while True:
+            choice = input(self.msg_txt).strip().casefold()
+            choice = self.unalias_choice(self.valid_choices, choice)
+            if choice in self.valid_choices:
+                return choice
+            if choice in Input.HELP_OPTIONS:
+                print(self.help_txt)
+            elif choice in Input.QUIT_OPTIONS:
+                print(self.goodbye)
+                quit()
+            else:
+                print(self.invalid_txt)
+
+        
+if __name__ == '__main__':
+    test = Input(valid_choices = ['rock', 'paper', 'scissors'], 
+                      msg_txt = 'R, P, or S? ', 
+                      invalid_txt = 'Not valid, sorry!', 
+                      help_txt = 'You need help', 
+                      delay = 0
+                      )
+    while True:
+        print(Input.PROMPT, test.get())
+    
