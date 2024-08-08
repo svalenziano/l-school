3d95c479058a6bf9821d6a68c8bdb2899dc929e5
SValenziano <senorvalenz@gmail.com>
2024-08-08 12:51:59 -0400

    Setting up for Lesson 9 problems

--- misc/left.py
+++ misc/left.py
@@ -1,39 +1,19 @@
-'''
-https://www.codewars.com/kata/65eb2c4c274bd91c27b38d32/train/python
-P
-    Input = two integers `n` and `m`
-    Output = sorted array of all ints `n` to `m` INCLUSIVE, 
-                which have exactly 3 divisors, 1 and the number excluded
-E
-D
-A
-    three_divisors [HELPER]
-        - for every num (excluding 1 and the num)
-            - include the number if a modulo operation results in 0 remainder
+class Vehicle:
+    WHEELS = 4
 
-    main_func
-        - for every num between n and m INCLUSIVE
-            - include the number if `three_divisors` returns True
-'''
-def has_three_divisors(num):
-    divisor_count = 0
-    halfway = int(num / 2)
-    for divisor in range(2, halfway + 1):
-        if num % divisor == 0:
-            divisor_count += 1
-            if divisor_count > 3:
-                return False
-    return divisor_count == 3
+    @classmethod
+    def wheels(cls):
+        return cls.WHEELS
 
+class Motorcycle(Vehicle):
+    @classmethod
+    def vehicle_wheels(cls):
+        return cls.WHEELS
 
-def solution(n, m):
-    result = [num
-              for num in range(n, m + 1)
-              if has_three_divisors(num)]
-    return result
+print((Motorcycle.mro()))
 
-print(solution(2, 100))
-print(solution(10000, 100000))
-print(solution(624, 625))
-print(solution(625, 626))
-print(solution(734, 735))
+print(Motorcycle.wheels())              # 4
+print(Motorcycle.WHEELS)                # 4
+print(Vehicle.wheels())                 # 4
+print(Vehicle.WHEELS)                   # 4
+print(Motorcycle.vehicle_wheels())      # 4
