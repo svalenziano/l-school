d6ff8249a0917de86df7b240254cee97982736aa
SValenziano <senorvalenz@gmail.com>
2024-08-09 13:29:28 -0400

    1st attempt

---
+++ py120/04_oo_practice_problems/03_Easy_2/p05.py
@@ -0,0 +1,17 @@
+class Greeting:
+    
+    def greet(self, message):
+        print(message)
+
+class Hello(Greeting):
+    
+    @classmethod
+    def hi(cls):
+        cls.greet('Hello')
+
+class Goodbye(Greeting):
+    def bye(self):
+        self.greet('Goodbye')
+
+h = Hello()
+h.hi()
