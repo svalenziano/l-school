7b0ee40e202546078040f5de4ecfef54d7e50e66
SValenziano <senorvalenz@gmail.com>
2024-08-12 09:22:04 -0400

    Part 1 WIP

--- py120/04_oo_practice_problems/05_Hard_1/p03.py
+++ py120/04_oo_practice_problems/05_Hard_1/p03.py
@@ -19,6 +19,10 @@ class Watercraft(FueledVehicleMixin):
         self.set_fuel_efficiency(fuel_efficiency)
         self.set_fuel_capacity(fuel_capacity)
 
+    def range(self):
+        return (self.fuel_capacity * self.fuel_efficiency) + 10
+
+
 class Motorboat(Watercraft):
     def __init__(self,
                  kilometers_per_liter,
@@ -41,3 +45,41 @@ class Catamaran(Watercraft):
                          kilometers_per_liter,
                          liters_of_fuel_capacity)
         
+class WheeledVehicle(FueledVehicleMixin):
+    def __init__(self,
+                 tire_list,
+                 kilometers_per_liter,
+                 liters_of_fuel_capacity):
+        self.tires = tire_list
+        self.set_fuel_efficiency(kilometers_per_liter)
+        self.set_fuel_capacity(liters_of_fuel_capacity)
+
+    def tire_pressure(self, tire_index):
+        self.tires[tire_index]
+
+    def inflate_tire(self, tire_index, pressure):
+        self.tires[tire_index] = pressure
+
+    
+
+class Auto(WheeledVehicle):
+    def __init__(self):
+        # 4 tires with various tire pressures
+        super().__init__([30, 30, 32, 32], 50, 25.0)
+
+class Motorcycle(WheeledVehicle):
+    def __init__(self):
+        # 2 tires with various tire pressures
+        super().__init__([20, 20], 80, 8.0)
+        
+marla = Auto()
+print(marla.range())
+
+hell_on_wheelz = Motorcycle()
+print(hell_on_wheelz.range())
+
+queen_mary = Catamaran(number_propellers=2,
+                       number_hulls=2,
+                       kilometers_per_liter=1.5,
+                       liters_of_fuel_capacity=600)
+print(queen_mary.range())
---
+++ py120/05_slightly_larger/01_tic_tac_toe/oop_ttt.py
@@ -0,0 +1,60 @@
+class TTTGame:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Board:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Row:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Square:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Marker:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Player:
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+
+class Human(Player):
+    def __init__(self):
+        pass
+    '''
+    
+    '''
+    def play(self):
+        pass
+
+    def mark(self):
+        pass
+
+class Computer(Player):
+    def __init__(self):
+        pass
+    '''
+    
+    '''
