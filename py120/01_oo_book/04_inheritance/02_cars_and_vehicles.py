b98dfa71f7ad9869f335cccd572f981d9f5b50ae
SValenziano <senorvalenz@gmail.com>
2024-08-17 07:20:42 -0400

    LS Solution

--- py120/01_oo_book/04_inheritance/02_cars_and_vehicles.py
+++ py120/01_oo_book/04_inheritance/02_cars_and_vehicles.py
@@ -1,40 +1,38 @@
 class Vehicle:
-
-    vehicle_counter = 0
+    number_of_vehicles = 0
 
     def __init__(self):
-        Vehicle.vehicle_counter += 1
+        Vehicle.number_of_vehicles += 1
 
     @classmethod
     def vehicles(cls):
-        return Vehicle.vehicle_counter
-
-    def vehicles_self(self):
-        return self.__class__.vehicles()
+        return Vehicle.number_of_vehicles
 
 class Car(Vehicle):
-    pass
+
+    def __init__(self):
+        super().__init__()
 
 class Truck(Vehicle):
-    pass
+
+    def __init__(self):
+        super().__init__()
 
 class Boat(Vehicle):
-    pass
 
+    def __init__(self):
+        super().__init__()
 
-print(f"{Car.vehicles()=}")     # 0
+print(Car.vehicles())     # 0
 car1 = Car()
-print(f"{Car.vehicles()=}")     # 1
+print(Car.vehicles())     # 1
 car2 = Car()
 car3 = Car()
 car4 = Car()
-print(f"{Car.vehicles()=}")     # 4
+print(Car.vehicles())     # 4
 truck1 = Truck()
 truck2 = Truck()
-print(f"{Truck.vehicles()=}")   # 6
+print(Truck.vehicles())   # 6
 boat1 = Boat()
 boat2 = Boat()
-print(f"{Boat.vehicles()=}")    # 8
-
-for i in [car1, car2, car3, car4, truck1, truck2, boat1, boat2]:
-    print(f"{i.__class__.__name__} {i.vehicles_self()=}")
+print(Boat.vehicles())    # 8
