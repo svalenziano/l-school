034995cf1b31daf8332bc51cbbb4e904affee28f
SValenziano <senorvalenz@gmail.com>
2024-08-17 07:04:34 -0400

    SV Experimention re: counter inheritance

--- py120/01_oo_book/04_inheritance/02_cars_and_vehicles.py
+++ py120/01_oo_book/04_inheritance/02_cars_and_vehicles.py
@@ -9,6 +9,8 @@ class Vehicle:
     def vehicles(cls):
         return Vehicle.vehicle_counter
 
+    def vehicles_self(self):
+        return self.__class__.vehicles()
 
 class Car(Vehicle):
     pass
@@ -20,17 +22,19 @@ class Boat(Vehicle):
     pass
 
 
-
-print(Car.vehicles())     # 0
+print(f"{Car.vehicles()=}")     # 0
 car1 = Car()
-print(Car.vehicles())     # 1
+print(f"{Car.vehicles()=}")     # 1
 car2 = Car()
 car3 = Car()
 car4 = Car()
-print(Car.vehicles())     # 4
+print(f"{Car.vehicles()=}")     # 4
 truck1 = Truck()
 truck2 = Truck()
-print(Truck.vehicles())   # 6
+print(f"{Truck.vehicles()=}")   # 6
 boat1 = Boat()
 boat2 = Boat()
-print(Boat.vehicles())    # 8
+print(f"{Boat.vehicles()=}")    # 8
+
+for i in [car1, car2, car3, car4, truck1, truck2, boat1, boat2]:
+    print(f"{i.__class__.__name__} {i.vehicles_self()=}")
