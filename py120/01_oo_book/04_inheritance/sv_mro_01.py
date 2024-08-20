dc639c8aa2bb80e52ae30c4842c789cebb9d0f21
SValenziano <senorvalenz@gmail.com>
2024-08-20 12:40:41 -0400

    Further simplified, removed `instances` list

--- py120/01_oo_book/04_inheritance/sv_mro_01.py
+++ py120/01_oo_book/04_inheritance/sv_mro_01.py
@@ -1,14 +1,11 @@
 class Mammal:
     instance_count = 0
-    instances = []
     def __init__(self, name):
         self._name = name
-        Mammal.instances.append(self)
         Mammal.instance_count += 1  
 
 class Dog(Mammal):
     pass
-
 
 luna = Dog('Luna')
 steven = Mammal("Steven")
