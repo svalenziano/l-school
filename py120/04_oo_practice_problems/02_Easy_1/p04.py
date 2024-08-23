f3d1837aa75f343a1ebc9735f6b305f4018aeb43
SValenziano <senorvalenz@gmail.com>
2024-08-23 17:43:24 -0400

    LS solution

--- py120/04_oo_practice_problems/02_Easy_1/p04.py
+++ py120/04_oo_practice_problems/02_Easy_1/p04.py
@@ -12,15 +12,13 @@ print(small_car.go_fast())
 
 
 '''
-When we called small_car.go_fast, you may have noticed that the output 
-includes the vehicle type. How is this done?
 
-On line 10, when we call `go_fast()`, Python looks for a `go_fast` method within 
-the Car class but does not find one.
 
-Therefore, it looks in the mixin class, and finds the method on line 2.  When
-this method is invoked, the value of `self` is the calling object, which is `small_car`.
+We use self.__class__.__name__ in the method. It works like so:
 
-Therefore, the value of `self.__class__.__name__` on line 3 will be the class name of 
-`small_car`, which is `Car`
+    self refers to the object referenced by small_car. In this case, that's a Car object.
+    self.__class__ returns a reference to the Car class, which is an object of type class.
+    Finally, self.__class__.__name__ returns the name of the Car class as a string: 'Car'.
+
+
 '''
