d0cbf0f2b231d24d765bf8b45ad83f948d84fcd6
SValenziano <senorvalenz@gmail.com>
2024-08-23 10:55:20 -0400

    Try 2

--- py120/03_more_oo/11_exceptions/p04.py
+++ py120/03_more_oo/11_exceptions/p04.py
@@ -1,8 +1,18 @@
+'''
+P
+Write a program that asks the user for a number. If the input isn't a number, let Python raise an appropriate exception. If the number is negative, raise a ValueError with an appropriate error message. If the number isn't negative, print a message that shows its value.
+E
+D
+A
+'''
+
 try:
-    num = float(input("Give me a positive number please. "))
+    num = float(input("Gimme a positive number pls "))
     if num < 0:
-        raise ValueError("Need positive number yo!")
+        raise ValueError("I said positive! Try again pls.")
+    if num % 1 == 0:
+        num = int(num)
 except ValueError as e:
-    print(f"ERROR: {e}")
+    print(f"{e.__class__.__name__}: {e}")
 else:
-    print(f"Your number is: {num}!!!")
+    print(f"Your number is ... {num}!!!")
--- py120/03_more_oo/11_exceptions/p05.py
+++ py120/03_more_oo/11_exceptions/p05.py
@@ -1,7 +1,22 @@
-class NegativeNumberError(ValueError):
-    pass
+'''
+P
+Modify your answer from the previous problem to raise a custom exception named NegativeNumberError instead of an ordinary ValueError exception.
+E
+D
+A
+'''
 
-num = float(input('Enter a number: '))
-if num < 0:
-    raise NegativeNumberError('Negative numbers are not allowed!')
-print(f'You entered {num}')
+class NegativeNumberError(ValueError):
+    def __init__(self, message="Number cannot be negative."):
+        super().__init__(message)
+
+try:
+    num = float(input("Gimme a positive number pls "))
+    if num < 0:
+        raise NegativeNumberError()
+    if num % 1 == 0:
+        num = int(num)
+except ValueError as e:
+    print(f"{e.__class__.__name__}: {e}")
+else:
+    print(f"Your number is ... {num}!!!")
