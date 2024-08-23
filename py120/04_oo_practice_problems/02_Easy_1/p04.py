30a03953c9de3be91f4684c4d3f4f35af00b0225
SValenziano <senorvalenz@gmail.com>
2024-08-23 17:43:05 -0400

    Try 2

--- py120/04_oo_practice_problems/02_Easy_1/p04.py
+++ py120/04_oo_practice_problems/02_Easy_1/p04.py
@@ -10,7 +10,17 @@ small_car = Car()
 print(small_car.go_fast())
 # I am a super fast Car!
 
+
 '''
-My answer
-self.__class__.__name__ returns the name of the calling class, in this case, 'Car'
+When we called small_car.go_fast, you may have noticed that the output 
+includes the vehicle type. How is this done?
+
+On line 10, when we call `go_fast()`, Python looks for a `go_fast` method within 
+the Car class but does not find one.
+
+Therefore, it looks in the mixin class, and finds the method on line 2.  When
+this method is invoked, the value of `self` is the calling object, which is `small_car`.
+
+Therefore, the value of `self.__class__.__name__` on line 3 will be the class name of 
+`small_car`, which is `Car`
 '''
