67d467b11f69954b609643e1b35f6e58546874dc
SValenziano <senorvalenz@gmail.com>
2024-08-31 10:32:55 -0400

    Working: while loop

--- py130/L1/6_higher_order_functions/p03.py
+++ py130/L1/6_higher_order_functions/p03.py
@@ -4,7 +4,26 @@ See [[Python, Reduce]] note
 Implement 'reduce'
 '''
 
+def reduce(callback, collection, start):
+    accumulator = start
+    idx = 0
+    while idx < len(collection):
+        element = collection[idx]
+        accumulator = callback(element, accumulator)
+        idx += 1
+    return accumulator
+
+
+numbers = (1, 2, 4, 8, 16)
+total = lambda number, accum: accum + number
+print(reduce(total, numbers, 0))        # 31
+
 numbers = [10, 3, 5]
 product = lambda number, accum: accum * number
-print(reduce(product, numbers, 2))     # 300
+print(reduce(product, numbers, 2))      # 300
+
+colors = ['red', 'orange', 'yellow', 'green',
+          'blue', 'indigo', 'violet']
+rainbow = lambda color, accum: accum + color[0].upper()
+print(reduce(rainbow, colors, ''))      # ROYGBIV
 
