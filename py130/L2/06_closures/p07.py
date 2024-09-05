b28322a9756607775a670638ffc814faacb9322a
SValenziano <senorvalenz@gmail.com>
2024-09-05 11:02:52 -0400

    My Solution

--- py130/L2/06_closures/p07.py
+++ py130/L2/06_closures/p07.py
@@ -2,10 +2,10 @@
 Write a function named later2 that takes two arguments: a function, func, and an argument for that function, first_arg. The return value should be a new function that takes an argument, second_arg. The new function should call the func with the arguments provided by first_arg and second_arg. Here's an example of how it might be used:
 '''
 
-
-
-
-
+def later2(func, first_arg):
+    def new_func(second_arg):
+        func(first_arg, second_arg)
+    return new_func
 
 def notify(message, when):
     print(f"{message} in {when} minutes!")
