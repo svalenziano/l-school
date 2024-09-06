c5034e10ecf62586b6482ba34690479cbea869cb
SValenziano <senorvalenz@gmail.com>
2024-09-06 12:39:03 -0400

    LS Solution

--- py130/exercises/01_easy_1/p08.py
+++ py130/exercises/01_easy_1/p08.py
@@ -7,12 +7,7 @@ Use a generator expression to yield each character of a string in reverse order.
 
 string = 'hello world!'
 
-start_idx = -1
-end_idx = -len(string) - 1
+reverse_generator = (string[i] for i in range(len(string) - 1, -1, -1))
 
-backwards = (string[idx]
-             for idx in range(start_idx, end_idx, -1))
-
-print(list(backwards))
-
-print(string[::-1])
+for char in reverse_generator:
+    print(char)
