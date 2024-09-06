44ed08de94453bcf4ce2e03bd6e819543d153844
SValenziano <senorvalenz@gmail.com>
2024-09-06 11:57:00 -0400

    LS Solution

--- py130/exercises/01_easy_1/p05.py
+++ py130/exercises/01_easy_1/p05.py
@@ -6,7 +6,7 @@ Remove all None values from a list using the filter method.
 
 lst = [42, 'hello', None, 'maybe', None, 'Its Friday 🥲']
 
-cleaned = list(filter(lambda x: x != None, lst))
+cleaned = list(filter(None, lst))
 
 print(lst)
 print(cleaned)
