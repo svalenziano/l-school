fcf5202c8bc03e07545a42a7e1f79657707f4753
SValenziano <senorvalenz@gmail.com>
2024-09-06 12:19:35 -0400

    Didn't work, but did something interesting

--- py130/L1/10_generators_practice_probs/p02.py
+++ py130/L1/10_generators_practice_probs/p02.py
@@ -2,9 +2,8 @@
 Create a generator function that generates the reciprocals of the numbers from 1 to n, where n is an argument to the function. Use a for loop to print each value.
 '''
 
-def reciprocals(n):
-    for num in range(1, n + 1):
+def reciprocals(max):
+    for num in range(1, max + 1):
         yield 1 / num
 
-for num in reciprocals(8):
-    print(num)
+print(list(reciprocals(10)))
