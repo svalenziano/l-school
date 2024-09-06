15227534286ec04114f32d31dd33944da19ebc35
SValenziano <senorvalenz@gmail.com>
2024-09-06 12:59:57 -0400

    LS Solution

--- py130/exercises/01_easy_1/p10.py
+++ py130/exercises/01_easy_1/p10.py
@@ -6,23 +6,14 @@ Create a generator function that yields user input strings until the word "stop"
 
 
 
-'''
-P
-E
-D
-A
-    - Get input from user
-    - If stop is entered, return
-'''
-
 
 def echo_generator():
     while True:
         _input = input("Gimme a word: ").casefold()
         if _input == 'stop':
             print('Bye!')
-            return False
-        print(f"I heard {_input}.")
-        # yield(_input)
+            break
+        yield(_input)
 
-echo_generator()
+for user_input in echo_generator():
+    print(user_input)
