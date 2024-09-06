65c9c1bb04125a3ae6c0d1d15aeddc66dc54533a
SValenziano <senorvalenz@gmail.com>
2024-09-06 12:40:19 -0400

    LS solution with annotation

--- py130/exercises/01_easy_1/p08.py
+++ py130/exercises/01_easy_1/p08.py
@@ -7,6 +7,8 @@ Use a generator expression to yield each character of a string in reverse order.
 
 string = 'hello world!'
 
+print(range(len(string) - 1, -1, -1))  # OMG duh, I was overthinking the indices
+
 reverse_generator = (string[i] for i in range(len(string) - 1, -1, -1))
 
 for char in reverse_generator:
