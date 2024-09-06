b7588a5efd3a2b10c2f39ac92921378a4df9d872
SValenziano <senorvalenz@gmail.com>
2024-09-06 11:50:10 -0400

    Standard function, not lambda

--- py130/exercises/01_easy_1/p03.py
+++ py130/exercises/01_easy_1/p03.py
@@ -6,9 +6,12 @@ Calculate the product of all numbers in a list using the reduce function.
 
 from functools import reduce
 
+def mult(num, accum):
+    return num * accum
+
 orig = list(range(1, 10))
 
-reduced = reduce(lambda num, accum: num * accum, orig)
+reduced = reduce(mult, orig)
 
 print(orig)
 print(reduced)
