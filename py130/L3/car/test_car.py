5631ad669288f3e45ad06037d365afd0c5ed08a6
SValenziano <senorvalenz@gmail.com>
2024-09-10 11:02:35 -0400

    Lesson 5, Assertions in unittest

--- py130/L3/car/test_car.py
+++ py130/L3/car/test_car.py
@@ -2,20 +2,32 @@ import unittest
 from car import Car
 
 class CarTest(unittest.TestCase):
+    def test_car_exists(self):
+        car = Car()
+        self.assertTrue(car is not None)
 
-    @unittest.skip
-    def test_wheels2(self):
-        car = Car()
-        self.assertEqual(3, car.wheels)
-    
-    @unittest.skip
-    def test_wheels3(self):
-        car = Car()
-        self.assertEqual(2, car.wheels)
-    
     def test_wheels(self):
         car = Car()
-        self.assertEqual(car.wheels, 4)
+        self.assertEqual(4, car.wheels)
+
+    def test_name_is_none(self):
+        car = Car()
+        self.assertIsNone(car.name)
+
+    def test_instance_of_car(self):
+        car = Car()
+        self.assertIsInstance(car, Car)
+
+    def test_includes_car(self):
+        car = Car()
+        lst = [1,2,3]
+        lst.append(car)
+        self.assertIn(car, lst)
+
+    @unittest.skip
+    def test_bad_wheels(self):
+        car = Car()
+        self.assertEqual(3, car.wheels)
 
 if __name__ == '__main__':
     unittest.main()
--- py130/temp.py
+++ py130/temp.py
@@ -1,2 +1,14 @@
-is_active = True
-assert not is_active, "Boolean check failed"
+def make_adder(n):
+    def adder(x):
+        return n + x
+
+    return adder
+
+adders = [make_adder(n) for n in range(1, 4)]
+
+add1, add2, add3 = adders
+
+print(add1(10))  # Output: 11
+print(add2(10))  # Output: 12
+print(add3(10))  # Output: 13
+print(add3.__closure__)
