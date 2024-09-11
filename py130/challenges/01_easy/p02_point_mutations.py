cf966c45501d7c430579d9ec54194993eaaa36e7
SValenziano <senorvalenz@gmail.com>
2024-09-11 13:32:00 -0400

    Start

---
+++ py130/challenges/01_easy/p02_point_mutations.py
@@ -0,0 +1,17 @@
+'''
+P
+    PROB: 
+        Calc Hamming dist btw two DNA Strands
+    In
+    Out
+    REQS
+        Ex
+            - Given two sequences of unequal length, calc, the distance over the shorter length
+        Impl
+    Qs
+E
+    See p02t.py
+D
+A
+
+'''
