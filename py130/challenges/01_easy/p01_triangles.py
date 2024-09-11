4582b1f8cf0066681e2370bbdcabff7fe13919c7
SValenziano <senorvalenz@gmail.com>
2024-09-11 12:33:20 -0400

    Attempt 1, PEDA complete

--- py130/challenges/01_easy/p01_triangles.py
+++ py130/challenges/01_easy/p01_triangles.py
@@ -3,17 +3,31 @@ P
     PROBLEM: Write a program to determine whether a triangle is equilateral, isosceles, or scalene.
 
     REQS, EXPLICIT
-        - An equilateral triangle has all three sides the same length.
-        - An isosceles triangle has exactly two sides of the same length.
-        - A scalene triangle has all sides of different lengths.
+        Triangle classifications
+            - An equilateral triangle has all three sides the same length.
+            - An isosceles triangle has exactly two sides of the same length.
+            - A scalene triangle has all sides of different lengths.
+        Side requirements
+            - For every side: length > 0
+            - For every combination of side lengths, side 1 + side 2 > side 3
 
-        Note: For a shape to be a triangle at all, all sides must be of length > 0, and the sum of the lengths of any two sides must be greater than the length of the third side.
+    REQS, IMPLICIT (examining tests):
+        Properties:
+            .kind
+                'equilateral'
+                'isosceles'
+                'scalene'
+        - raise ValueError if side length is <= 0
+        - raise ValueError if triangle isn't valid (side 1 + side 2 > side 3)
 
-    REQS, IMPLICIT:
 
 E
-    
+    See /p01_triangles_tests.py
 D
+    Custom Triangle class
+        sides = list of floats/ints
+        .kind
+
 A
 '''
 
