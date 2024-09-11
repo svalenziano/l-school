482d8a04cabdb8b0a3d6042dc0303a06d92628fb
SValenziano <senorvalenz@gmail.com>
2024-09-11 12:12:53 -0400

    fix filenames

--- py130/challenges/01_easy/p01_triangles_tests.py
+++ py130/challenges/01_easy/p01_triangles_tests.py
@@ -1,24 +1,6 @@
-'''
-P
-    PROBLEM: Write a program to determine whether a triangle is equilateral, isosceles, or scalene.
-
-    REQS, EXPLICIT
-        - An equilateral triangle has all three sides the same length.
-        - An isosceles triangle has exactly two sides of the same length.
-        - A scalene triangle has all sides of different lengths.
-
-        Note: For a shape to be a triangle at all, all sides must be of length > 0, and the sum of the lengths of any two sides must be greater than the length of the third side.
-
-    REQS, IMPLICIT:
-
-E
-D
-A
-'''
-
 # LS PROVIDED TESTS
 import unittest
-from triangle import Triangle
+from p01_triangles import Triangle
 
 class TestTriangle(unittest.TestCase):
     @unittest.skip
