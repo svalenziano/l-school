4a3b26afe99836386f836893566b678ebb4b69fe
SValenziano <senorvalenz@gmail.com>
2024-09-13 14:29:14 -0400

    Working :)

--- py130/regex_book/p02.py
+++ py130/regex_book/p02.py
@@ -1,12 +1,11 @@
 # SV CODE
+import re
 
-
-
-
-
+def fields(text):
+    return re.split(r'[ ,\t]+', text, flags=re.VERBOSE)
 
 # LS TESTS
-fields("Pete,201,Student");    # ['Pete', '201', 'Student']
-fields("Pete \t 201   ,  TA"); # ['Pete', '201', 'TA']
-fields("Pete \t 201");         # ['Pete', '201']
-fields("Pete \n 201");         # ['Pete', '\n', '201']
+print(fields("Pete,201,Student"))       # ['Pete', '201', 'Student']
+print(fields("Pete \t 201   ,  TA"))    # ['Pete', '201', 'TA']
+print(fields("Pete \t 201"))            # ['Pete', '201']
+print(fields("Pete \n 201"))            # ['Pete', '\n', '201']
