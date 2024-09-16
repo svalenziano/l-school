54e8f89889ac707833069423ed4cca0d6d29b513
SValenziano <senorvalenz@gmail.com>
2024-09-16 13:19:10 -0400

    Start

---
+++ py130/challenges/01_easy/p04_anagrams.py
@@ -0,0 +1,9 @@
+'''
+Write a program that takes a word and a list of possible anagrams and selects the correct sub-list that contains the anagrams of the word.
+
+For example, given the word "listen" and a list of candidates like "enlists", "google", "inlets", and "banana", the program should return a list containing "inlets". Please read the test suite for the exact rules of anagrams.
+
+'''
+
+class Anagram:
+    pass
