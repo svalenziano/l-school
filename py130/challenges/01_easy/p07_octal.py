f1e8583833a99ed91f3584ac5bf98b37c8fc583c
SValenziano <senorvalenz@gmail.com>
2024-09-17 16:57:08 -0400

    Before solving

---
+++ py130/challenges/01_easy/p07_octal.py
@@ -0,0 +1,11 @@
+'''
+Implement octal to decimal conversion. Given an octal input string, your program should produce a decimal output. Treat invalid input as octal 0. Note that the only valid digits in an octal number are 0, 1, 2, 3, 4, 5, 6, and 7.
+
+'''
+
+class Octal:
+    def __init__(self):
+        pass
+
+    def to_decimal(self):
+        pass
