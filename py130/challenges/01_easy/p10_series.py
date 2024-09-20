c1367021ba57688ee52367b9c3992669bd87e1ea
SValenziano <senorvalenz@gmail.com>
2024-09-20 07:57:31 -0400

    PEDAC Complete

--- py130/challenges/01_easy/p10_series.py
+++ py130/challenges/01_easy/p10_series.py
@@ -15,3 +15,28 @@ Likewise, here are the 4-digit series:
 Finally, if you ask for a 6-digit series from a 5-digit string, you should throw an error.
 '''
 
+class Series:
+    def __init__(self, num:str):
+        '''
+        INPUT: Numeric string
+        ALGO
+            typecheck
+        '''
+        pass
+
+    def slices(self, length:int):
+        '''
+        INPUT: Integer = length of slice
+        OUTPUT: List of list of integers
+            List of all possible CONSECUTIVE slices of the string.  Each 'slice'
+                is a list of integers.
+            If `length` is equal to len(string), return a single slice, equivalent to the string
+            Raise ValueError if `length` is greater than the length of the string
+        ALGO:
+            Check length and raise ValueError if appropriate
+            Return nested comprehension
+        '''
+        pass
+
+if __name__ == '__main__':
+    pass
