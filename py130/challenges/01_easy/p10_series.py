8c5e5cae1b4d12d28ef42679ac115da7d4529500
SValenziano <senorvalenz@gmail.com>
2024-09-23 16:00:26 -0400

    Cleanup and remove PEDAC

--- py130/challenges/01_easy/p10_series.py
+++ py130/challenges/01_easy/p10_series.py
@@ -1,20 +1,3 @@
-'''
-Write a program that will take a string of digits and return all the possible consecutive number series of a specified length in that string.
-
-For example, the string "01234" has the following 3-digit series:
-
-    012
-    123
-    234
-
-Likewise, here are the 4-digit series:
-
-    0123
-    1234
-
-Finally, if you ask for a 6-digit series from a 5-digit string, you should throw an error.
-'''
-
 import tomllib
 
 class Series:
@@ -23,11 +6,6 @@ class Series:
         MESSAGES = tomllib.load(f)
     
     def __init__(self, numeric_string:str):
-        '''
-        INPUT: Numeric string
-        ALGO
-            typecheck
-        '''
         if not isinstance(numeric_string, str):
             raise TypeError(self.MESSAGES['error_numeric_str'])
         
@@ -39,17 +17,6 @@ class Series:
         self._numeric_string = numeric_string
 
     def slices(self, slice_length:int):
-        '''
-        INPUT: Integer = length of slice
-        OUTPUT: List of list of integers
-            List of all possible CONSECUTIVE slices of the string.  Each 'slice'
-                is a list of integers.
-            If `length` is equal to len(string), return a single slice, equivalent to the string
-            Raise ValueError if `length` is greater than the length of the string
-        ALGO:
-            Check length and raise ValueError if appropriate
-            Return nested comprehension
-        '''
         nums = [int(digit) for digit in self._numeric_string]
 
         if slice_length > len(nums):
@@ -57,9 +24,3 @@ class Series:
         
         return [nums[start_idx: start_idx + slice_length]
                 for start_idx in range(len(nums) - slice_length + 1)]
-
-
-if __name__ == '__main__':
-    # Series('1234')
-    # Series('abcd')  # should 
-    pass
