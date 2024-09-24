9314d0993fa84e0b3f9faa73b57039a971a5f985
SValenziano <senorvalenz@gmail.com>
2024-09-24 16:23:34 -0400

    Prior to solving

--- py130/temp.py
+++ py130/temp.py
@@ -1,3 +1,17 @@
-import random as r
+def my_decorator(func):
+    some_var = 5
+    first, *middle, last = list(range(43))
+    def wrapper():
+        print("Before the function call")
+        print(some_var); print(first, middle, last)
+        func()
+        print("After the function call")
 
-print(r.randint(1, 100))
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+print(say_hello.__closure__)
+print(hex(id(say_hello)))
