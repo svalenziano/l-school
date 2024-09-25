2f59f3a138d9c164b22314fd4bafdef7acc9170f
SValenziano <senorvalenz@gmail.com>
2024-09-25 14:05:10 -0400

    Done w PEDAC

--- py130/challenges/02_medium/p03_clock.py
+++ py130/challenges/02_medium/p03_clock.py
@@ -7,3 +7,40 @@ Two clock objects that represent the same time should be equal to each other.
 
 You may not use any built-in date or time functionality; just use arithmetic operations.
 '''
+from __future__ import annotations
+
+class Clock:
+    def at(self, hrs, mins=0):
+        '''
+        returns clock object
+        '''
+        pass
+
+    def __str__(self):
+        pass
+
+    def __add__(self, minutes:int):
+        '''
+        OUTPUT: Return new clock object
+        REQS:
+            - Wrap around at midnight, both forwards and backwards
+            - Handle adding more than one day's worth of minutes
+        '''
+        pass
+
+    def __sub__(self, minutes:int):
+        '''
+        return new clock object
+        REQS:
+            - same as __add__
+        '''
+        pass
+
+    def __eq__(self, other_clock:Clock):
+        '''
+        
+        '''
+        pass
+
+
+    
