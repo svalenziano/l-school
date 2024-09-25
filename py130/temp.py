b04cdb2f346b8f058806d60f612c37bfaad8b496
SValenziano <senorvalenz@gmail.com>
2024-09-25 13:57:19 -0400

    Before solving

--- py130/temp.py
+++ py130/temp.py
@@ -1,17 +1,6 @@
-def my_decorator(func):
-    some_var = 5
-    first, *middle, last = list(range(43))
-    def wrapper():
-        print("Before the function call")
-        print(some_var); print(first, middle, last)
-        func()
-        print("After the function call")
-
-    return wrapper
-
-@my_decorator
-def say_hello():
-    print("Hello!")
-
-print(say_hello.__closure__)
-print(hex(id(say_hello)))
+x = None or 10 and 1/0
+print(x)
+# print(False or (None and 3) or (10 and 'False'))
+#         False or    None    or        'False'
+#                                     'False'
+print(type(x))
