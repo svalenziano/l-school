8750a3235d7e2557c9115ac8df720db98d024365
SValenziano <senorvalenz@gmail.com>
2024-09-26 07:23:50 -0400

    decorator, first attempt

--- py130/temp.py
+++ py130/temp.py
@@ -1,6 +1,14 @@
-x = None or 10 and 1/0
+def my_decorator(func, *args, **kwargs):
+    def wrapper():
+        print("Calling the func")
+        func(*args, **kwargs)
+        print("Wrapping things up")
+    return wrapper
+
+@my_decorator
+def fancy_sort(lst_to_reverse:list, reverse=False):
+    return sorted(lst_to_reverse, reverse=reverse)
+
+my_list = list('The quick brown fox jumps over the lazy dog.')
+x = fancy_sort(my_list, reverse=True)
 print(x)
-# print(False or (None and 3) or (10 and 'False'))
-#         False or    None    or        'False'
-#                                     'False'
-print(type(x))
