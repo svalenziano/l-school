9a386bb04e0588ecded6268de5a316871545dc8b
SValenziano <senorvalenz@gmail.com>
2024-10-04 12:31:18 -0400

    Lesson 8

---
+++ ls170/simple_http_server/http_server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function server () {
+
+while true
+do
+    read message
+    echo "You said $message."
+done
+
+}
+
+coproc SERVER_PROCESS { server; }
+
+netcat -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
