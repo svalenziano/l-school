adb3c2f08072679077b141f1be077f090989709a
SValenziano <senorvalenz@gmail.com>
2024-10-29 16:33:30 -0400

    Before '11. Using Layouts'

--- py175/03_flask/book_viewer_starter/app.py
+++ py175/03_flask/book_viewer_starter/app.py
@@ -2,15 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+with open('book_viewer/data/toc.txt', 'r', encoding='utf-8') as f:
+    toc = f.readlines()
+
 @app.route("/")
 def index():
-    with open('book_viewer/data/toc.txt', 'r', encoding='utf-8') as f:
-        toc = f.readlines()
-
-    # for idx, line in enumerate(toc):
-    #     toc[idx] = '<a href="#" class="pure-menu-link">' + line + '</a>'
-    
     return render_template('index.html', contents=toc)
+
+@app.route('/chapters/1')
+def chap1():
+    with open('book_viewer/data/chp1.txt', 'r') as f:
+        chapter = f.readlines()
+
+    return render_template('chapters.html', 
+                           chapter=chapter,
+                           contents=toc,
+                           title='Chapter 1',)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
