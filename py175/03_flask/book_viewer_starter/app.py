bd661b2388b9b61cf4888dd2c4ab427d28f46b86
SValenziano <senorvalenz@gmail.com>
2024-10-29 15:40:48 -0400

    Before '10. Adding a Chapter Page'

--- py175/03_flask/book_viewer_starter/app.py
+++ py175/03_flask/book_viewer_starter/app.py
@@ -4,7 +4,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    with open('book_viewer/data/toc.txt', 'r', encoding='utf-8') as f:
+        toc = f.readlines()
+
+    # for idx, line in enumerate(toc):
+    #     toc[idx] = '<a href="#" class="pure-menu-link">' + line + '</a>'
+    
+    return render_template('index.html', contents=toc)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
