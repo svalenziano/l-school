779988c9c626ccc90150997a35d89a8b434cf4f3
SValenziano <senorvalenz@gmail.com>
2024-10-30 15:21:01 -0400

    Before `13. Before request`

--- py175/03_flask/book_viewer_starter/app.py
+++ py175/03_flask/book_viewer_starter/app.py
@@ -3,21 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 with open('book_viewer/data/toc.txt', 'r', encoding='utf-8') as f:
-    toc = f.readlines()
+    lst_of_chapter_names = f.readlines()
 
 @app.route("/")
 def index():
-    return render_template('home.html', contents=toc, title="Table of Contents")
+    return render_template('home.html', 
+                           contents=lst_of_chapter_names, 
+                           title="Table of Contents")
 
-@app.route('/chapters/<num>')
-def chap(num):
+@app.route('/disp-chap/<num>')
+def chapters(num):
     with open(f'book_viewer/data/chp{num}.txt', 'r') as f:
         chapter = f.readlines()
 
     return render_template('chapters.html', 
                            chapter=chapter,
-                           contents=toc,
-                           title=f'Chapter {num}',)
+                           contents=lst_of_chapter_names,
+                           title=f'Chapter {num}',
+                           chap_name=lst_of_chapter_names[int(num) - 1])
 
 @app.route("/show/<name>/<content>")
 def show(name, content):
