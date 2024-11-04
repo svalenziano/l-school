256d8a3770c631ac271b4a0ad35722943e1d5db1
SValenziano <senorvalenz@gmail.com>
2024-11-04 12:35:12 -0500

    Before '7. add a new todolist'

--- py175/04_todo_app/02_todo/app.py
+++ py175/04_todo_app/02_todo/app.py
@@ -1,20 +1,28 @@
-from flask import Flask, render_template, g, url_for
+from flask import Flask, render_template, g, url_for, request
 
 app = Flask(__name__)
+
+# Dummy (defualt) lists
+lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
 
 @app.route("/")
 def index():
     return app.redirect('/lists')
 
-@app.route('/lists')
+@app.get('/lists')
 def lists():
-
-    lists = [
-        {"title": "Lunch Groceries", "todos": []},
-        {"title": "Dinner Groceries", "todos": []},
-    ]
-
     return render_template('lists.html', lists=lists)
+
+@app.post('/lists')
+def lists_post():
+    return 'Radical'
+
+@app.route('/lists/new')
+def new_list():
+    return render_template('new_list.html')
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
