c99bc1f6a4433a5fff11a25a33b5332a6afee73b
SValenziano <senorvalenz@gmail.com>
2024-11-04 14:34:04 -0500

    Before implementing UUID

--- py175/04_todo_app/02_todo/app.py
+++ py175/04_todo_app/02_todo/app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, g, url_for, request
+from flask import (
+    Flask, 
+    render_template, 
+    g, 
+    url_for, 
+    request, 
+    session
+)
 
 app = Flask(__name__)
+app.secret_key='secret1'  # WARNING, INSECURE!
 
 # Dummy (defualt) lists
 lists = [
@@ -8,17 +16,26 @@ lists = [
         {"title": "Dinner Groceries", "todos": []},
     ]
 
+@app.before_request
+def before_request():
+    # Check session for list info
+    if 'lists' not in session:
+        session['lists'] = []
+
 @app.route("/")
 def index():
     return app.redirect('/lists')
 
 @app.get('/lists')
 def lists():
-    return render_template('lists.html', lists=lists)
+    return render_template('lists.html', lists=session['lists'])
 
 @app.post('/lists')
 def lists_post():
-    return 'Radical'
+    new_list = request.form['list_title']
+    session['lists'].append({'title': new_list, 'todos': []})
+    session.modified = True
+    return app.redirect('lists')
 
 @app.route('/lists/new')
 def new_list():
