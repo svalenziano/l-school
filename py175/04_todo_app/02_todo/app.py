ab53b42063e922f3d59f70771212fec7920937d3
SValenziano <senorvalenz@gmail.com>
2024-11-04 11:57:26 -0500

    WIP, delete Poetry lockfile

--- py175/04_todo_app/02_todo/app.py
+++ py175/04_todo_app/02_todo/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, g, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    return app.redirect('/lists')
+
+@app.route('/lists')
+def lists():
+
+    lists = [
+        {"title": "Lunch Groceries", "todos": []},
+        {"title": "Dinner Groceries", "todos": []},
+    ]
+
+    return render_template('lists.html', lists=lists)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
