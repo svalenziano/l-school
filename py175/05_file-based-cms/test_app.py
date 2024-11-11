2a0476ec3b4e52b0c36e7537d298366c3e27eaf6
SValenziano <senorvalenz@gmail.com>
2024-11-11 15:40:25 -0500

    LS Solution

--- py175/05_file-based-cms/test_app.py
+++ py175/05_file-based-cms/test_app.py
@@ -1,40 +1,43 @@
 import unittest
-import os
-from flask import Flask
 from app import app
 
-    
+class CMSTest(unittest.TestCase):
 
-class TestApp(unittest.TestCase):
     def setUp(self):
-        app.testing = True
+        app.config['TESTING'] = True
         self.client = app.test_client()
 
     def test_index(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('about.txt', response.get_data(as_text=True))
 
-    def test_file(self):
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content_type, "text/html; charset=utf-8")
+        self.assertIn("about.txt", response.get_data(as_text=True))
+        self.assertIn("changes.txt", response.get_data(as_text=True))
+        self.assertIn("history.txt", response.get_data(as_text=True))
+
+    def test_viewing_text_document(self):
         with self.client.get('/history.txt') as response:
             self.assertEqual(response.status_code, 200)
-            self.assertIn('Guido van Rossum', response.get_data(as_text=True))
+            self.assertEqual(response.content_type, "text/plain; charset=utf-8")
+            self.assertIn("Python 0.9.0 (initial release) is released.",
+                          response.get_data(as_text=True))
 
-    def test_unsafe_paths(self):
-        response = self.client.get('/..')
-        self.assertEqual(response.status_code, 302)
-        response = self.client.get('/../')
-        self.assertEqual(response.status_code, 404)
-        response = self.client.get('/../..')
-        self.assertEqual(response.status_code, 404)
+    def test_document_not_found(self):
+        # Attempt to access a nonexistent file and verify a redirect happens
+        with self.client.get("/notafile.ext") as response:
+            self.assertEqual(response.status_code, 302)
 
-    def test_nonexistent(self):
-        response = self.client.get('/abc.txt')
-        self.assertEqual(response.status_code, 302)
-        response = self.client.get('/lorem_ipsum')
-        self.assertEqual(response.status_code, 302)
-        response = self.client.get('/fake.json')
-        self.assertEqual(response.status_code, 302)
+        # Verify redirect and message handling works
+        with self.client.get(response.headers['Location']) as response:
+            self.assertEqual(response.status_code, 200)
+            self.assertIn("notafile.ext does not exist",
+                          response.get_data(as_text=True))
+
+        # Assert that a page reload removes the message
+        with self.client.get("/") as response:
+            self.assertNotIn("notafile.ext does not exist",
+                             response.get_data(as_text=True))
 
 if __name__ == '__main__':
     unittest.main()
