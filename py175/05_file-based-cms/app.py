4d106af815ff4994ea6c8ef0cf959816179e8f7c
SValenziano <senorvalenz@gmail.com>
2024-11-11 13:02:43 -0500

    Working, but not using `send_from_directory`

--- py175/05_file-based-cms/app.py
+++ py175/05_file-based-cms/app.py
@@ -1,16 +1,36 @@
 from flask import (Flask, 
-                   render_template)
+                   render_template,
+                   g)
 import os
 import os.path
+from functools import wraps
+
+def get_data_dir(f):
+    '''
+    Get path to data directory and store in `g.data_dir`
+    '''
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        root = os.path.abspath(os.path.dirname(__file__))
+        g.data_dir = os.path.join(root, "cms/data")
+        return f(*args, **kwargs)
+    return wrapper
 
 app = Flask(__name__)
 
 @app.route("/")
+@get_data_dir
 def index():
-    root = os.path.abspath(os.path.dirname(__file__))
-    data_dir = os.path.join(root, "cms/data")
-    filenames = os.listdir(data_dir) 
+    filenames = os.listdir(g.data_dir) 
     return render_template('index.html', filenames = filenames)
+
+@app.route('/<filename>')
+@get_data_dir
+def file(filename):
+    filepath = os.path.join(g.data_dir, filename)
+    with open(filepath, 'r') as f:
+        content = f.readlines()
+    return render_template('file.html', content=content)
 
 if __name__ == "__main__":
     x = os.environ.get('LS_DEV_MACHINE')  # requires `export LS_DEV_MACHINE=true` in .bashrc
