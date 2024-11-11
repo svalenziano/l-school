565e839cb573cfe03c74b8acd4def36c66bb4dd8
SValenziano <senorvalenz@gmail.com>
2024-11-11 12:13:29 -0500

    Remove Zone Identifiers.  IMplementing `3. Adding an index page`.  Working SV solution

--- py175/05_file-based-cms/app.py
+++ py175/05_file-based-cms/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import (Flask, 
+                   render_template)
+import os
+import os.path
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>This is a todo starter!</h1>"
+    data_dir = os.path.join(os.path.abspath(app.root_path), "./cms/data")
+    filenames = os.listdir(data_dir) 
+    return render_template('index.html', filenames = filenames)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5003)
+    x = os.environ.get('LS_DEV_MACHINE')  # requires `export LS_DEV_MACHINE=true` in .bashrc
+    if x:
+        print("🔴")
+        app.run(debug=True, port=5003)
+    else:
+        app.run(debug=False)
