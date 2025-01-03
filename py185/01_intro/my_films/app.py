8a419182f68f79dc17392f6ad05c5e4d0f712a92
SValenziano <senorvalenz@gmail.com>
2025-01-03 07:46:26 -0500

    Done w Lesson 5

--- py185/01_intro/my_films/app.py
+++ py185/01_intro/my_films/app.py
@@ -1,16 +1,18 @@
 import psycopg2
 from psycopg2 import extras
 
+connection = psycopg2.connect(dbname='ls185_1_4')
+
 try:
-    with psycopg2.connect(dbname='ls185_1_4') as connection:
+    with connection:
         with connection.cursor(cursor_factory=extras.DictCursor) as cursor:
             cursor.execute('''
-                            SELECT * FROM films JOIN directors
-                            ON films.director_id = directors.id
-                            WHERE name = 'Sidney Lumet';
-            ''')
-            x = cursor.fetchall()
-            print(x)
+                            SELECT genre, count(id) FROM films
+                            WHERE duration < 110
+                            GROUP BY genre;''')
+            films = cursor.fetchall()
 finally:
     connection.close()
-    
+
+for film in films:
+    print(f"{film['genre'].rjust(10)} | {film['count']}")
