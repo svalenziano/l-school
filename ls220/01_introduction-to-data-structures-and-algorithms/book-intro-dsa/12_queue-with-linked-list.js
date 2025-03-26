e8c56d55262b9374e1e5c7a407128780bbb97888
SValenziano <senorvalenz@gmail.com>
2025-03-26 13:13:26 -0400

    before looking at LS solution

--- ls220/01_introduction-to-data-structures-and-algorithms/book-intro-dsa/12_queue-with-linked-list.js
+++ ls220/01_introduction-to-data-structures-and-algorithms/book-intro-dsa/12_queue-with-linked-list.js
@@ -14,31 +14,45 @@ class Queue {
     // Returns the value of the top most element without removing it.
 
     // If the queue is empty, it returns `null`.
-    return this.front;
+    if (this.front) return this.front.val
+    return null;
   }
 
   enqueue(value) {
     // Adds an item to the queue
     // create a new node
+    let n = new ListNode(value)
     // If there's a back of the line
+    if (this.back) {
       // point that node towards the new node
+      this.back.next = n;
+    }
     // set the new node as the `back` (regardless)
+    this.back = n;
     // if there's not a front of the line,
+    if (!this.front) {
       // set the new node as the `front`, too
+      this.front = n;
+    }
   }
 
   dequeue() {
     // Removes the item from the queue and returns it
     // if `front`:
+    if (this.front) {
       // create `oldFront` to hold current `front` value
+      let oldFront = this.front;
       // if `front` has a `next` value:
+      if (this.front.next) {
         // point `front` towards 2nd-in-line
-      // else:
-        // set `front` to `null`
-        // set `back` to `null`
-      // return `oldFront`
-    // Else:
-      // returns `null`.
+        this.front = this.front.next;
+      } else {
+        this.front = null;
+        this.back = null;
+      }
+      return oldFront.val;
+    }
+    return null;
   }
 }
 
