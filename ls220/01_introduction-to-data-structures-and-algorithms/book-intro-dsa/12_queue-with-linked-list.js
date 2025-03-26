51cca731d48fce5c3f4c1957ab846d2b8c777460
SValenziano <senorvalenz@gmail.com>
2025-03-26 12:45:45 -0400

    Queue - before solving

---
+++ ls220/01_introduction-to-data-structures-and-algorithms/book-intro-dsa/12_queue-with-linked-list.js
@@ -0,0 +1,43 @@
+class ListNode {
+  constructor(val = 0, next = null) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+  }
+  peek() {
+    // Returns the value of the top most element without removing it.
+
+    // If the queue is empty, it returns `null`.
+  }
+
+  enqueue(value) {
+    // Adds an item to the queue
+  }
+
+  dequeue() {
+    // Removes the item from the queue and returns it
+
+    // If the queue is empty, it returns `null`.
+  }
+}
+
+const myQueue = new Queue();
+myQueue.enqueue(1);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.enqueue(2);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.enqueue(3);
+console.log('Front element:', myQueue.peek());  // logs 'Front element: 1'
+myQueue.dequeue();
+console.log('Front element after dequeue:', myQueue.peek());  // logs 'Front element after dequeue: 2'
+myQueue.dequeue();
+console.log('Front element after dequeue:', myQueue.peek());  // logs 'Front element after dequeue: 3'
+myQueue.dequeue();
+console.log('Peek on empty queue:', myQueue.peek());  // logs 'Peek on empty queue: null'
+console.log('`back` on empty queue:', myQueue.back);  // logs '`back` on empty queue: null'
