6469c9f35d2c5810e70b1c5ee49a09fe5873c814
SValenziano <senorvalenz@gmail.com>
2025-04-26 09:14:55 -0400

    10.02 WIP

--- ls220/10_miscellaneous-problems/02_merge-overlapping-intervals.js
+++ ls220/10_miscellaneous-problems/02_merge-overlapping-intervals.js
@@ -17,41 +17,40 @@
 // Output: [[3,9]]
 // Explanation: All intervals overlap and are merged into a single interval.
 
-function mergeIntervals(intervals) {
-  // Your implementation here
-}
+
 
 // Test Cases
-console.log(mergeIntervals([[7,8], [1,3], [6,11], [2,4]]));
-// Expected: [[1,4], [6,11]]
+// console.log(mergeIntervals([[7,8], [1,3], [6,11], [2,4]]));
+// // Expected: [[1,4], [6,11]]
 
 console.log(mergeIntervals([[2,5], [4,8], [10,12], [13,16]]));
 // Expected: [[2,8], [10,12], [13,16]]
 
-console.log(mergeIntervals([[3,6], [3,4], [5,8], [7,9]]));
-// Expected: [[3,9]]
+// console.log(mergeIntervals([[3,6], [3,4], [5,8], [7,9]]));
+// // Expected: [[3,9]]
 
-console.log(mergeIntervals([[1,3], [5,7], [9,11]]));
-// Expected: [[1,3], [5,7], [9,11]]
+// console.log(mergeIntervals([[1,3], [5,7], [9,11]]));
+// // Expected: [[1,3], [5,7], [9,11]]
 
-console.log(mergeIntervals([[1,4], [0,4]]));
-// Expected: [[0,4]]
+// console.log(mergeIntervals([[1,4], [0,4]]));
+// // Expected: [[0,4]]
 
-console.log(mergeIntervals([[1,4], [2,3]]));
-// Expected: [[1,4]]
+// console.log(mergeIntervals([[1,4], [2,3]]));
+// // Expected: [[1,4]]
 
-console.log(mergeIntervals([]));
-// Expected: []
+// console.log(mergeIntervals([]));
+// // Expected: []
 
-console.log(mergeIntervals([[1,4], [4,5]]));
-// Expected: [[1,5]]
+// console.log(mergeIntervals([[1,4], [4,5]]));
+// // Expected: [[1,5]]
         
         
 /*
 P
 //////////////////////////////////////////////////////
 
-
+ - Given 2 intervals:
+  - if the beginning of interval 1 is sooner than the end of interval 2, then they are separate
 E
 //////////////////////////////////////////////////////
 My examples & tests:
@@ -66,11 +65,45 @@ D
 
 A
 //////////////////////////////////////////////////////
+- create deep copy of nested array
+- anchor and runner pointers at 0 and 1
+- while anchor is less than last element (must have at least 2 to compare):
+  - Should intervals be merged?
+    - yes: remove both intervals (splice) and unshift a new interval into the beginning of the array.  Move anchor and runner back to home positions (0 and 1)
+    - no: move runner
+  - if runner is beyond end of list (=== length):
+    - increment the anchor and move the runner to anchor + 1
+- return result
 
+how to tell if intervals should be merged?
+  - end of first is greater than beginning of second
+  - end of second is greater than beginning of first
 
 */
 
-// LS TESTS
 
+function mergeIntervals(intervals) {
+  intervals = JSON.parse(JSON.stringify(intervals));
+  let anchor = 0;
+  let runner = 1;
+
+  // merge
+  let aStart = intervals[anchor][0];
+  let aEnd = intervals[anchor][intervals[anchor].length - 1]
+  let rStart = intervals[runner][0];
+  let rEnd = intervals[runner][intervals[runner].length - 1]
+
+
+  if (aStart >= rStart && aStart <= rEnd ||  // end of first >=  beginning of second
+      rStart >= aStart && rStart <= aEnd) {
+    console.log("overlap!")
+    let min = Math.min(aStart, rStart);
+    let max = Math.max(aEnd, rEnd);
+    intervals.splice(anchor, 1);
+    intervals.splice(runner - 1, 1);
+    intervals.unshift([min, max]);
+    }
+  return intervals
+}
 
 
