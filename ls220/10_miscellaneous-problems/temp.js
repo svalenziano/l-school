6634fc4517a877def0bbe0f3c326400d6d26cc19
SValenziano <senorvalenz@gmail.com>
2025-04-28 10:43:21 -0400

    temp working

--- ls220/10_miscellaneous-problems/temp.js
+++ ls220/10_miscellaneous-problems/temp.js
@@ -1,43 +1,54 @@
-/*
-Given an array of values, return a two-element array: the first element is the index of the first '3', and the second element is the index of the last '3'.  Return `[-1, -1]` if no 3's are present in the input array.
+/* 
+In this problem, you're given an array of numbers nums, and a specific integer k. Your objective is to compute the average value of each contiguous subarray of length k within the given array.
 
 Requirements:
-	Time complexity O(logN)
-	Space complexity: no requirement.
 
+    The input will be an array of numbers and an integer k.
+    You need to find the average of every contiguous subarray of size k in the array.
+    The output should be an array containing these averages.
+
+Example test cases: 
 */
 
-// console.log(findRangeOfThrees([1, 2, 3, 3, 3, 3, 3, 4, 5])); // [2, 6]
-// console.log(findRangeOfThrees([1, 2, 5, 5, 6, 9, 10]));      // [-1, -1]
-// console.log(findRangeOfThrees([]));                          // [-1, -1]
-
-
+console.log(findAverages([1, 2, 3, 4, 5, 6], 3)); // [ 2, 3, 4, 5 ]
+console.log(findAverages([1, 2, 3, 4, 5], 2));    // [1.5, 2.5, 3.5, 4.5]
+console.log(findAverages([10, 20, 30, 40, 50], 4)); // [ 25, 35 ]
+console.log(findAverages([5, 5, 5, 5, 5], 1));      // [ 5, 5, 5, 5, 5 ]
+console.log(findAverages([1, 3, 2, 6, -1, 4, 1, 8, 2], 5)); // [2.2, 2.8, 2.4, 3.6, 2.8]
 
 
 /* 
-v1 idea: binary search for the first 3, then binary search for the last 3
+IDEA: Make a window of size k (create sum as you go).  While the right side of the window is less than the length of the array, push the "window average" to `result`, then move the window right by one index (subtract left value, move left, add right value, move right).  Return result.
 
+How to find the average?
+IDEA: Maintain a 'running sum' of the window.  With each move of the window, drop the leftmost value, and add the rightmost value.  Average is `sum / k`
 
-
-
+ALGO:
+  - 
 */
 
-function findSmallest(arr, target) {
+function findAverages(arr, k) {
   let l = 0;
-  let r = arr.length - 1;
-  while (l <= r) {
-    let mid = Math.floor((l + r) / 2);
-    if (arr[mid] >= target) {
-      r = mid - 1;
-    } else {
-      l = mid + 1;
-    }
+  let r = 0;
+  let windowSum = arr[0];
+  let result = [];
+  
+  // setup the window
+  for (let i = 1; i < k; i++) {
+    r += 1;
+    windowSum += arr[r];
   }
-  return arr[r + 1] === target? r + 1 : -1;
+
+  while (r < arr.length) {
+    // console.log('l:', l, ', r:', r, 'windowSum: ', windowSum)
+    result.push(windowSum / k)
+
+    //update the window
+    windowSum -= arr[l]
+    l += 1;
+    r += 1;
+    windowSum += arr[r];
+  }
+  return result;
 }
 
-console.log(findSmallest([1,2,3], 3))  // 2
-console.log(findSmallest([1,2,3,3,3,3], 3))  // 2
-console.log(findSmallest([1, 2, 3, 3, 3, 3, 3, 4, 5], 3))  // 2
-console.log(findSmallest([], 3))  // -1
-console.log(findSmallest([1, 2], 3))  // -1
