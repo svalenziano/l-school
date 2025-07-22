67c2ffa06545e0d2c52ef0470e08a8d6a8fc8638
SValenziano <senorvalenz@gmail.com>
2025-07-22 15:01:05 -0400

    working, before refactoring

--- js230/02-events/02-12_guessing-game/script.js
+++ js230/02-events/02-12_guessing-game/script.js
@@ -3,9 +3,12 @@
 const SUCCESS = 0;
 const TOO_HIGH = 1;
 const TOO_LOW = -1;
+const INVALID = 'invalid';
 
 class Game {
-  
+
+  static MAX_GUESS = 100;
+  static MIN_GUESS = 1;
 
   static randomIntInclusive(low, high) {
     // returns random integer, inclusive of `low` and high` args
@@ -18,37 +21,67 @@ class Game {
     return Math.floor(Math.random() * (high - low + 1) + low)
   }
 
-  constructor() {
+  constructor({guessBox, guessButton, messageDisplay}) {
+    messageDisplay.textContent = "Guess a number from 1 to 100"
+    guessBox.autocomplete = "off";
+    guessButton.removeAttribute("disabled");
+
     this.guessCount = 0;
-    this.secretNumber = Game.randomIntInclusive(1, 100);
+    this.secretNumber = Game.randomIntInclusive(Game.MIN_GUESS, Game.MAX_GUESS);
     this.gameOver = false;
-    this.guessBox = document.querySelector("#guessBox");
-    this.guessButton = document.querySelector("#guessButton");
-    this.messageDisplay = document.querySelector("#message");
+    this.guessBox = guessBox;
+    this.guessButton = guessButton;
+    this.messageDisplay = messageDisplay;
+    this.resetGuessBox();
     console.log(`NEW GAME! Secret number = ${this.secretNumber}.`)
   }
 
 
+  resetGuessBox() {
+    guessBox.value = "";
+    guessBox.focus();
+  }
+
   handleGuessClick() {
     let result = this.checkGuess(this.guessBox.value);
-    if (result === 0) {
+
+    if (result === SUCCESS) {
       this.messageDisplay.textContent = `Wow! ${this.guessCount} guesses and you win!`
-    } else if (result === -1) {
+      this.guessButton.setAttribute("disabled", "")
+    } else if (result === TOO_LOW) {
       this.messageDisplay.textContent = 'Too low.  Try again...'
-    } else {
+    } else if (result === TOO_HIGH) {
       this.messageDisplay.textContent = 'Too high.  Try again...'
+    } else if (result === INVALID) {
+      this.messageDisplay.textContent = "Invalid input: please provide a number between 1 and 100, inclusive."
+      this.guessBox.select();
+    } else {
+      throw new Error("Unexpected value for 'result'")
     }
   }
 
   checkGuess(guess) {
     guess = Number(guess);
+    console.log(`guess = ${guess}`)
+
+    if (
+      (!Number.isInteger(guess)) ||
+      guess < Game.MIN_GUESS ||
+      guess > Game.MAX_GUESS
+      ) {
+        return INVALID;
+    }
+
     this.guessCount += 1;
+
     if (guess === this.secretNumber) {
       this.gameOver = true;
       return SUCCESS;
     } else if (guess > this.secretNumber) {
+      this.resetGuessBox();
       return TOO_HIGH;
     } else {
+      this.resetGuessBox();
       return TOO_LOW;
     }
   }
@@ -57,11 +90,21 @@ class Game {
 
 // EVENTS & GAMEPLAY
 document.addEventListener("DOMContentLoaded", (e) => {
-  let game = new Game();
 
+  const messageDisplay = document.querySelector("#message");
+  const guessBox = document.querySelector("#guessBox");
+  const guessButton = document.querySelector("#guessButton");
+  const newGameButton = document.querySelector("#newGame");
+  
+  let game = new Game({guessBox, guessButton, messageDisplay});
 
   guessButton.addEventListener("click", (e) => {
     e.preventDefault();
     game.handleGuessClick();
-  })
-})
+  });
+
+  newGameButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    game = new Game({guessBox, guessButton, messageDisplay});
+  });
+});
