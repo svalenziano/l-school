ca5abaffd144eba2d29633e069b911c4c8b306bf
SValenziano <senorvalenz@gmail.com>
2025-07-23 08:01:27 -0400

    Guessing game done.  Before starting on "Build an Input Box"

--- js230/02-events/02-12_guessing-game/script.js
+++ js230/02-events/02-12_guessing-game/script.js
@@ -1,14 +1,13 @@
 "use strict";
 
-const SUCCESS = 0;
-const TOO_HIGH = 1;
-const TOO_LOW = -1;
-const INVALID = 'invalid';
-
 class Game {
 
   static MAX_GUESS = 100;
   static MIN_GUESS = 1;
+  static SUCCESS = 0;
+  static TOO_HIGH = 1;
+  static TOO_LOW = -1;
+  static INVALID = 'invalid';
 
   static randomIntInclusive(low, high) {
     // returns random integer, inclusive of `low` and high` args
@@ -38,21 +37,21 @@ class Game {
 
 
   resetGuessBox() {
-    guessBox.value = "";
-    guessBox.focus();
+    this.guessBox.value = "";
+    this.guessBox.focus();
   }
 
   handleGuessClick() {
     let result = this.checkGuess(this.guessBox.value);
 
-    if (result === SUCCESS) {
+    if (result === Game.SUCCESS) {
       this.messageDisplay.textContent = `Wow! ${this.guessCount} guesses and you win!`
       this.guessButton.setAttribute("disabled", "")
-    } else if (result === TOO_LOW) {
+    } else if (result === Game.TOO_LOW) {
       this.messageDisplay.textContent = 'Too low.  Try again...'
-    } else if (result === TOO_HIGH) {
+    } else if (result === Game.TOO_HIGH) {
       this.messageDisplay.textContent = 'Too high.  Try again...'
-    } else if (result === INVALID) {
+    } else if (result === Game.INVALID) {
       this.messageDisplay.textContent = "Invalid input: please provide a number between 1 and 100, inclusive."
       this.guessBox.select();
     } else {
@@ -69,20 +68,20 @@ class Game {
       guess < Game.MIN_GUESS ||
       guess > Game.MAX_GUESS
       ) {
-        return INVALID;
+        return Game.INVALID;
     }
 
     this.guessCount += 1;
 
     if (guess === this.secretNumber) {
       this.gameOver = true;
-      return SUCCESS;
+      return Game.SUCCESS;
     } else if (guess > this.secretNumber) {
       this.resetGuessBox();
-      return TOO_HIGH;
+      return Game.TOO_HIGH;
     } else {
       this.resetGuessBox();
-      return TOO_LOW;
+      return Game.TOO_LOW;
     }
   }
 }
