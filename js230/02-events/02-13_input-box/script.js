503c0776c8900767ffc2caeeb1edd7ab1b73da29
SValenziano <senorvalenz@gmail.com>
2025-07-23 08:22:47 -0400

    working

--- js230/02-events/02-13_input-box/script.js
+++ js230/02-events/02-13_input-box/script.js
@@ -1,16 +1,22 @@
 document.addEventListener("DOMContentLoaded", (e) => {
   const textField = document.querySelector(".text-field");
+  let cursorID;
 
   document.addEventListener("click", (e) => {
     if (e.target === textField) {
       textField.classList.add("focused");
+
+      cursorID = setInterval(() => {
+        textField.classList.toggle("cursor");
+      }, 500);
+
     } else {
       textField.classList.remove("focused");
+      clearInterval(cursorID);
+      textField.classList.remove("cursor");
     }
   });
 
-  setInterval(() => {
-    textField.classList.toggle("cursor");
-  }, 500);
+  
 
 })
