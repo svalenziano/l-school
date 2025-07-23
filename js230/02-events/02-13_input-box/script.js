1eff60d61d6afc011478203a80dbc5f6a1bf0012
SValenziano <senorvalenz@gmail.com>
2025-07-23 08:27:11 -0400

    after refactor - working

--- js230/02-events/02-13_input-box/script.js
+++ js230/02-events/02-13_input-box/script.js
@@ -1,20 +1,34 @@
+
+// EVENTS AND APP LOGIC
 document.addEventListener("DOMContentLoaded", (e) => {
   const textField = document.querySelector(".text-field");
   let cursorID;
 
   document.addEventListener("click", (e) => {
     if (e.target === textField) {
-      textField.classList.add("focused");
-
-      cursorID = setInterval(() => {
-        textField.classList.toggle("cursor");
-      }, 500);
+      focusTextBox();
+      flashCursor();
 
     } else {
-      textField.classList.remove("focused");
-      clearInterval(cursorID);
-      textField.classList.remove("cursor");
+      unfocusTextBox();
     }
+
+    // helpers
+      function focusTextBox() {
+        textField.classList.add("focused");
+      }
+
+      function flashCursor() {
+        cursorID = setInterval(() => {
+          textField.classList.toggle("cursor");
+        }, 500);
+      }
+
+      function unfocusTextBox() {
+        textField.classList.remove("focused");
+        clearInterval(cursorID);
+        textField.classList.remove("cursor");
+      }
   });
 
   
