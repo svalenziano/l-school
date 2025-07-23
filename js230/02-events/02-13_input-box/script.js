e6d1ff0f44f810121cb13529a1ebd914700d236f
SValenziano <senorvalenz@gmail.com>
2025-07-23 08:16:35 -0400

    Step 3 implemented

--- js230/02-events/02-13_input-box/script.js
+++ js230/02-events/02-13_input-box/script.js
@@ -1,1 +1,16 @@
-console.log("hello world!")
+document.addEventListener("DOMContentLoaded", (e) => {
+  const textField = document.querySelector(".text-field");
+
+  document.addEventListener("click", (e) => {
+    if (e.target === textField) {
+      textField.classList.add("focused");
+    } else {
+      textField.classList.remove("focused");
+    }
+  });
+
+  setInterval(() => {
+    textField.classList.toggle("cursor");
+  }, 500);
+
+})
