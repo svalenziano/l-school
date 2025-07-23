11f9ae5756df64f0b6088d74834ddd6ce0743d08
SValenziano <senorvalenz@gmail.com>
2025-07-23 08:59:05 -0400

    6 solved w SV solution, prior to refactor to simplify

--- js230/02-events/02-13_input-box/script.js
+++ js230/02-events/02-13_input-box/script.js
@@ -2,6 +2,7 @@
 // EVENTS AND APP LOGIC
 document.addEventListener("DOMContentLoaded", (e) => {
   const textField = document.querySelector(".text-field");
+  const contentField = document.querySelector(".content");
   let cursorID;
 
   document.addEventListener("click", (e) => {
@@ -13,7 +14,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
       unfocusTextBox();
     }
 
-    // helpers
+    // HELPERS
       function focusTextBox() {
         textField.classList.add("focused");
       }
@@ -29,6 +30,43 @@ document.addEventListener("DOMContentLoaded", (e) => {
         clearInterval(cursorID);
         textField.classList.remove("cursor");
       }
+
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (!textBoxIsFocused()) return;
+
+    if (e.key === "Backspace") {
+      const text = contentField.textContent;
+      contentField.textContent = text.slice(0, -1);
+      return;
+    }
+
+    if (keyShouldBeIgnored()) {
+      contentField.textContent += e.key;
+    }
+
+    // HELPERS
+    function keyShouldBeIgnored() {
+      const keysToIgnore = [
+        'Shift', 
+        'Control', 
+        'Unidentified', 
+        'Meta', 
+        'Alt', 
+        'CapsLock',
+        'Home',
+        'End',
+        'Insert',
+        'Delete',
+        'Backspace'
+        ];
+      return !keysToIgnore.includes(e.key)
+    }
+
+    function textBoxIsFocused() {
+      return textField.classList.contains("focused");
+    }
   });
 
   
