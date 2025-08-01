04b08a5996fcce3968a500e1e61ea5a62c8ff570
SValenziano <senorvalenz@gmail.com>
2025-08-01 13:48:50 -0400

    02 working - CSS positioning next

--- js230/small_problems/03_misc-gui-apps/02_exotic-animals/my-solve.js
+++ js230/small_problems/03_misc-gui-apps/02_exotic-animals/my-solve.js
@@ -8,10 +8,28 @@ class MagicCaption {
   constructor(parent) {
     this.parent = parent;
     this.hideAll();
+
+    for (let img of parent.querySelectorAll("img")) {
+      img.addEventListener("mouseenter", (e) => {
+        this.show(this.getFigcaptionSibling(e.target));
+      })
+
+      img.addEventListener("mouseleave", (e) => {
+        this.hide(this.getFigcaptionSibling(e.target));
+      })
+    }
+  }
+
+  getFigcaptionSibling(element) {
+    return element.parentNode.querySelector("figcaption")
   }
 
   hide(element) {
     element.classList.add("hidden");
+  }
+
+  show(element) {
+    element.classList.remove("hidden");
   }
 
   hideAll() {
