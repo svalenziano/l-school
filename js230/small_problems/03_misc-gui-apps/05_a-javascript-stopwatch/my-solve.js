32337b220beeeb6b78e5218969f0ee80d538f8b0
SValenziano <senorvalenz@gmail.com>
2025-08-05 16:24:40 -0400

    Stopwatch WIP

--- js230/small_problems/03_misc-gui-apps/05_a-javascript-stopwatch/my-solve.js
+++ js230/small_problems/03_misc-gui-apps/05_a-javascript-stopwatch/my-solve.js
@@ -3,8 +3,9 @@
 class Stopwatch {
   
   constructor(parentElement) {
-    this.elapsedMillis = 0;
-    this.startTime = null;
+    this.lastStartTime = null;
+    this.elapsedAtLastPause = 0;
+    this.currentInterval = null;
     this.running = false;
     
     if (Array.from(parentElement.children).length > 0) {
@@ -16,10 +17,11 @@ class Stopwatch {
       <div class="time-display fira-code-medium"><time>00:00:00:00</time></div>
       <div class="controls">
         <button class="start-stop">Start</button>
-        <button class="reset">Reset</button>
+        <button class="reset inactive">Reset</button>
       </div>
     </div>`
 
+    this.$timeDisplay = parentElement.querySelector(".time-display time");
     this.$startStopButton = parentElement.querySelector(".start-stop")
     this.$resetButton = parentElement.querySelector(".reset")
     
@@ -27,32 +29,54 @@ class Stopwatch {
     this.$resetButton.addEventListener("click", this.handleResetClick.bind(this))
   }
 
+  calcElapsedTime() {
+    return this.elapsedAtLastPause + (new Date() - this.lastStartTime);
+  }
+
   reset() {
-    this.startTime = new Date();
+    this.pause();
+    this.deactivateButton(this.$resetButton);
+    this.lastStartTime = new Date();
+    this.elapsedAtLastPause = 0;
   }
 
   start() {
-    this.reset();
+    this.activateButton(this.$resetButton);
     this.running = true;
-    this.$startStopButton.textContent = "Start"
+    this.lastStartTime = new Date();
+    this.$startStopButton.textContent = "Pause"
+    this.currentInterval = setInterval(() => {
+      this.$timeDisplay.textContent = this.calcElapsedTime();
+    }, 100);
   }
 
-  stop() {
-    this.$startStopButton.textContent = "Stop"
+  pause() {
+    this.$startStopButton.textContent = "Start"
+    this.elapsedAtLastPause = this.calcElapsedTime();
     this.running = false;
+    clearInterval(this.currentInterval);
+    this.currentInterval = null;
   }
 
   handleStartStopClick(e) {
     console.log(e)
     if (this.running === true) {
-      this.stop();
+      this.pause();
     } else {
       this.start();
     }
   }
 
   handleResetClick(e) {
-    console.log(e)
+    this.reset();
+  }
+
+  activateButton(buttonEle) {
+    buttonEle.classList.remove("inactive");
+  }
+
+  deactivateButton(buttonEle) {
+    buttonEle.classList.add("inactive");
   }
 }
 
