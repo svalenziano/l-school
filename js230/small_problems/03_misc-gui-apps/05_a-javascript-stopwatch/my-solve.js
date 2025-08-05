48f48f2b3faa1ea3f612994037fe9bbf9861424f
SValenziano <senorvalenz@gmail.com>
2025-08-05 15:46:37 -0400

    05 WIP

--- js230/small_problems/03_misc-gui-apps/05_a-javascript-stopwatch/my-solve.js
+++ js230/small_problems/03_misc-gui-apps/05_a-javascript-stopwatch/my-solve.js
@@ -1,2 +1,62 @@
 "use strict";
 
+class Stopwatch {
+  
+  constructor(parentElement) {
+    this.elapsedMillis = 0;
+    this.startTime = null;
+    this.running = false;
+    
+    if (Array.from(parentElement.children).length > 0) {
+      throw new Error("parentElement should not contain any elements")
+    }
+
+    parentElement.innerHTML = `<div>
+    
+      <div class="time-display fira-code-medium"><time>00:00:00:00</time></div>
+      <div class="controls">
+        <button class="start-stop">Start</button>
+        <button class="reset">Reset</button>
+      </div>
+    </div>`
+
+    this.$startStopButton = parentElement.querySelector(".start-stop")
+    this.$resetButton = parentElement.querySelector(".reset")
+    
+    this.$startStopButton.addEventListener("click", this.handleStartStopClick.bind(this))
+    this.$resetButton.addEventListener("click", this.handleResetClick.bind(this))
+  }
+
+  reset() {
+    this.startTime = new Date();
+  }
+
+  start() {
+    this.reset();
+    this.running = true;
+    this.$startStopButton.textContent = "Start"
+  }
+
+  stop() {
+    this.$startStopButton.textContent = "Stop"
+    this.running = false;
+  }
+
+  handleStartStopClick(e) {
+    console.log(e)
+    if (this.running === true) {
+      this.stop();
+    } else {
+      this.start();
+    }
+  }
+
+  handleResetClick(e) {
+    console.log(e)
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const sw = new Stopwatch(document.querySelector(".stopwatch"))
+})
