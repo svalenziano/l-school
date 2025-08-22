55042feeac2d9374af95b5e82d7dbd1a9e85655a
SValenziano <senorvalenz@gmail.com>
2025-08-22 10:44:46 -0400

    After step 2

--- js235/03_http-in-js/08_fetch/script.js
+++ js235/03_http-in-js/08_fetch/script.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  let store = document.getElementById('store');
+  let $store = document.getElementById('store');
   let response = await fetch('https://ls-230-web-store-demo.herokuapp.com/products');
-  store.innerHTML = await response.text();
+  $store.innerHTML = await response.text();
+
+  $store.addEventListener("click", async (event) => {
+    event.preventDefault();
+    target = event.target;
+    if (target.tagName !== "A") return;
+    
+    const url = 'https://ls-230-web-store-demo.herokuapp.com' + target.getAttribute("href")
+    const response = await fetch(url);
+    $store.innerHTML = await response.text();
+  })
+
 })
