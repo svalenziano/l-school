4655d5821044645c280dba99aa8d26dee956dacd
SValenziano <senorvalenz@gmail.com>
2025-08-28 15:38:05 -0400

    Part 2 complete!  Working but need to refactor...

--- js235/04_put-it-together/01_photo-gallery/public/javascripts/gallery.js
+++ js235/04_put-it-together/01_photo-gallery/public/javascripts/gallery.js
@@ -11,20 +11,38 @@ class Gallery
 */
 
 class Gallery {
-  constructor({$slidesDiv, $infoHeader}) {
+  constructor({$slidesDiv, $infoHeader, $commentUL}) {
     this.$slidesDiv = $slidesDiv;
     this.$infoHeader = $infoHeader;
+    this.$commentUL = $commentUL
   }
 
   async init() {
-    // try {
+    const FIRST_PHOTO_INDEX = 0;
 
-    // } catch (error) {
-
-    // }
+    // POPULATE PHOTOS & INFO HEADER WITH FIRST PHOTO
+    // fetch photos
     const photosResponse = await fetch("./photos")
     const photosArray = await photosResponse.json()
+    console.log(photosArray);
+
+    // render photos and information
     this.$slidesDiv.innerHTML = templates.photos(photosArray);
+    this.$infoHeader.innerHTML = templates.photoInformation(
+      photosArray[FIRST_PHOTO_INDEX]
+    );
+
+    // const firstID = Math.min(photosArray.map((obj) => obj.id))
+    const firstID = photosArray[FIRST_PHOTO_INDEX]['id']
+
+    // POPULATE COMMENTS FOR FIRST PHOTO
+    // fetch comments
+    const firstCommentResponse = await fetch(`./comments?photo_id=${firstID}`);
+    const commentsArray = await firstCommentResponse.json()
+    console.log(commentsArray)
+
+    // render comments
+    this.$commentUL.innerHTML = templates.comments(commentsArray)
   }
 }
 
@@ -32,7 +50,8 @@ document.addEventListener("DOMContentLoaded", async () => {
   let gal = new Gallery({
       $slidesDiv: document.querySelector("div#slides"),
       $infoHeader: document.querySelector("header#information"),
+      $commentUL: document.querySelector("div#comments ul")
   })
 
-  await gal.init();
+  gal.init();
 })
